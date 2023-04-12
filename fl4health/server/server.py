16c2993af9f3bd802571f3e320856fdaa6e8c098
jewelltaylor <taylorjewell@outlook.com>
2023-04-11 20:15:10 -0400

    Remove redundant if condition and user super class fit

--- fl4health/server/server.py
+++ fl4health/server/server.py
@@ -1,5 +1,4 @@
 import concurrent.futures
-import timeit
 from logging import INFO
 from typing import List, Optional, Tuple, Union
 
@@ -29,7 +28,6 @@ class ClientLevelDPWeightedFedAvgServer(Server):
 
     def fit(self, num_rounds: int, timeout: Optional[float]) -> History:
         """Run federated averaging for a number of rounds."""
-        history = History()
 
         # Poll clients for sample counts
         log(INFO, "Polling Clients for sample counts")
@@ -41,65 +39,11 @@ class ClientLevelDPWeightedFedAvgServer(Server):
 
         sample_counts: List[int] = [int(result[1].properties["num_samples"]) for result in results]
 
-        # If Client Level DP and Weighted FedAvg, set sample counts to compute client weights
-        if isinstance(self.strategy, ClientLevelDPFedAvgM) and self.strategy.weighted_averaging:
+        # If Weighted FedAvg, set sample counts to compute client weights
+        if self.strategy.weighted_averaging:
             self.strategy.sample_counts = sample_counts
 
-        # Initialize parameters
-        log(INFO, "Initializing global parameters")
-        self.parameters = self._get_initial_parameters(timeout=timeout)
-        log(INFO, "Evaluating initial parameters")
-        res = self.strategy.evaluate(0, parameters=self.parameters)
-        if res is not None:
-            log(
-                INFO,
-                "initial parameters (loss, other metrics): %s, %s",
-                res[0],
-                res[1],
-            )
-            history.add_loss_centralized(server_round=0, loss=res[0])
-            history.add_metrics_centralized(server_round=0, metrics=res[1])
-
-        # Run federated learning for num_rounds
-        log(INFO, "FL starting")
-        start_time = timeit.default_timer()
-
-        for current_round in range(1, num_rounds + 1):
-            # Train model and replace previous global model
-            res_fit = self.fit_round(server_round=current_round, timeout=timeout)
-            if res_fit:
-                parameters_prime, _, _ = res_fit  # fit_metrics_aggregated
-                if parameters_prime:
-                    self.parameters = parameters_prime
-
-            # Evaluate model using strategy implementation
-            res_cen = self.strategy.evaluate(current_round, parameters=self.parameters)
-            if res_cen is not None:
-                loss_cen, metrics_cen = res_cen
-                log(
-                    INFO,
-                    "fit progress: (%s, %s, %s, %s)",
-                    current_round,
-                    loss_cen,
-                    metrics_cen,
-                    timeit.default_timer() - start_time,
-                )
-                history.add_loss_centralized(server_round=current_round, loss=loss_cen)
-                history.add_metrics_centralized(server_round=current_round, metrics=metrics_cen)
-
-            # Evaluate model on a sample of available clients
-            res_fed = self.evaluate_round(server_round=current_round, timeout=timeout)
-            if res_fed:
-                loss_fed, evaluate_metrics_fed, _ = res_fed
-                if loss_fed:
-                    history.add_loss_distributed(server_round=current_round, loss=loss_fed)
-                    history.add_metrics_distributed(server_round=current_round, metrics=evaluate_metrics_fed)
-
-        # Bookkeeping
-        end_time = timeit.default_timer()
-        elapsed = end_time - start_time
-        log(INFO, "FL finished in %s", elapsed)
-        return history
+        return super().fit(num_rounds=num_rounds, timeout=timeout)
 
 
 def _handle_finished_future_after_poll(
