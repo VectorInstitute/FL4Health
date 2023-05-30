a50abc2498d0987ec4c00cc8c59c623986687bf8
Yuchong Zhang <yuchong.zhang@vectorinstitute.ai>
2023-05-30 12:39:12 -0400

    added sampler arg for data loading functions

--- fl4health/utils/load_data.py
+++ fl4health/utils/load_data.py
@@ -1,6 +1,6 @@
 from logging import INFO
 from pathlib import Path
-from typing import Dict, Tuple
+from typing import Dict, Optional, Tuple
 
 import torchvision.transforms as transforms
 from flwr.common.logger import log
@@ -14,7 +14,7 @@ from fl4health.utils.sampler import LabelBasedSampler
 def load_mnist_data(
     data_dir: Path,
     batch_size: int,
-    sampler: LabelBasedSampler,
+    sampler: Optional[LabelBasedSampler] = None,
 ) -> Tuple[DataLoader, DataLoader, Dict[str, int]]:
     """Load MNIST Dataset (training and validation set)."""
     log(INFO, f"Data directory: {str(data_dir)}")
@@ -26,8 +26,10 @@ def load_mnist_data(
     )
     train_ds: BaseDataset = MNISTDataset(data_dir, train=True, transform=transform)
     val_ds: BaseDataset = MNISTDataset(data_dir, train=False, transform=transform)
-    train_ds = sampler.subsample(train_ds)
-    val_ds = sampler.subsample(val_ds)
+
+    if sampler is not None:
+        train_ds = sampler.subsample(train_ds)
+        val_ds = sampler.subsample(val_ds)
 
     train_loader = DataLoader(train_ds, batch_size=batch_size, shuffle=True)
     validation_loader = DataLoader(val_ds, batch_size=batch_size)
@@ -36,7 +38,9 @@ def load_mnist_data(
     return train_loader, validation_loader, num_examples
 
 
-def load_cifar10_data(data_dir: Path, batch_size: int) -> Tuple[DataLoader, DataLoader, Dict[str, int]]:
+def load_cifar10_data(
+    data_dir: Path, batch_size: int, sampler: Optional[LabelBasedSampler] = None
+) -> Tuple[DataLoader, DataLoader, Dict[str, int]]:
     """Load CIFAR-10 (training and validation set)."""
     log(INFO, f"Data directory: {str(data_dir)}")
     transform = transforms.Compose(
@@ -47,6 +51,11 @@ def load_cifar10_data(data_dir: Path, batch_size: int) -> Tuple[DataLoader, Data
     )
     training_set = CIFAR10(str(data_dir), train=True, download=True, transform=transform)
     validation_set = CIFAR10(str(data_dir), train=False, download=True, transform=transform)
+
+    if sampler is not None:
+        training_set = sampler.subsample(training_set)
+        validation_set = sampler.subsample(validation_set)
+
     train_loader = DataLoader(training_set, batch_size=batch_size, shuffle=True)
     validation_loader = DataLoader(validation_set, batch_size=batch_size)
     num_examples = {
