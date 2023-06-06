aa8b403f7aa0a988eddf435f385167fb20b802cd
David Emerson <43939939+emersodb@users.noreply.github.com>
2023-06-06 09:03:06 -0400

    Unifying the holdout evaluation script

--- research/flamby/fed_isic2019/README.md
+++ research/flamby/fed_isic2019/README.md
@@ -4,27 +4,28 @@ The hyperparameter sweep performs five training runs for each pair of parameters
 
 To run this on a particular collection of five runs (i.e. for fixed set of hyperparameters) the command
 ``` bash
-python -m research.flamby.fed_isic2019.fedprox.evaluate_on_holdout_global.py \
+python -m research.flamby.fed_isic2019.fedprox.evaluate_on_holdout.py \
     --artifact_dir path/to/runs/to/analyze/ \
     --dataset_dir path/to/fedisic/datasets/ \
     --eval_write_path path/to/write/eval/results/to.txt
+    --eval_global_model
 ```
 
 An example command is
 ``` bash
-python -m research.flamby.fed_isic2019.fedprox.evaluate_on_holdout_global.py \
+python -m research.flamby.fed_isic2019.fedprox.evaluate_on_holdout.py \
     --artifact_dir research/flamby/fed_isic2019/fedprox/hp_sweep_results/mu_0.01_lr_0.0001/ \
     --dataset_dir /Users/david/Desktop/FLambyDatasets/fedisic2019/ \
     --eval_write_path research/flamby/fed_isic2019/fedprox/test_eval_results.txt
+    --eval_global_model
 
 ```
 
 __NOTE__: You must have the correct venv activated for this to run. See the [FLamby readme](/research/flamby/README.md) for guidance.
 
 
-#### Two evaluation scripts
+#### Two evaluation modes
 
-There are two different evaluation scripts with slightly different aims.
+There are two different evaluation modes with slightly different aims that are toggled by the flag `--eval_global_model`. Inclusion of the flag indicates that there are both "local" model checkpoints and "global" model checkpoints. With the flag activated, the script will evaluate both types of models and write out performance. Examples of methods that have global models are FedProx, FedAvg, and Scaffold.
 
-* `evaluate_on_holdout_global.py` is meant to be run for FL methods that have a "global" model that works for all clients and "local" checkpoints based on best performance on local data. Such methods are FedProx, FedAvg, and Scaffold.
-* `evaluate_on_holdout_personal.py` is meant to be run for FL methods that only have "local" models. That is, there is no "functioning" global model. Methods like APFL and FENDA only have a notion of "local" models because a portion of the model is specific to each client.
+Running the script without the `--eval_global_model` flag will only search for and evaluate "local" models. That is, models specific to each client. Methods like APFL and FENDA only have a notion of "local" models because a portion of the model is specific to each client.
---
+++ research/flamby/fed_isic2019/evaluate_on_holdout.py
@@ -0,0 +1,145 @@
+import argparse
+from logging import INFO
+from typing import Dict
+
+import torch
+from flamby.datasets.fed_isic2019 import BATCH_SIZE, NUM_CLIENTS, FedIsic2019
+from flwr.common.logger import log
+from torch.utils.data import DataLoader
+
+from research.flamby.fed_isic2019.utils import (
+    FedIsic2019Metric,
+    evaluate_model,
+    get_all_run_folders,
+    get_metric_avg_std,
+    load_global_model,
+    load_local_model,
+    write_measurement_results,
+)
+
+
+def main(artifact_dir: str, dataset_dir: str, eval_write_path: str, eval_global_model: bool) -> None:
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    all_run_folder_dir = get_all_run_folders(artifact_dir)
+    test_results: Dict[str, float] = {}
+    metrics = [FedIsic2019Metric()]
+
+    all_local_test_metrics = {run_folder_dir: 0.0 for run_folder_dir in all_run_folder_dir}
+    all_server_test_metrics = {run_folder_dir: 0.0 for run_folder_dir in all_run_folder_dir}
+
+    # First we test each client's best model on local test data and the best server model on that same data
+
+    for client_number in range(NUM_CLIENTS):
+        client_test_dataset = FedIsic2019(center=client_number, train=False, pooled=False, data_path=dataset_dir)
+        test_loader = DataLoader(client_test_dataset, batch_size=BATCH_SIZE, shuffle=False)
+
+        local_test_metrics = []
+        server_test_metrics = []
+        for run_folder_dir in all_run_folder_dir:
+            local_model = load_local_model(run_folder_dir, client_number)
+            local_run_metric = evaluate_model(local_model, test_loader, metrics, device)
+            log(
+                INFO,
+                f"Client Number {client_number}, Run folder: {run_folder_dir}: "
+                f"Local Model Test Performance: {local_run_metric}",
+            )
+            local_test_metrics.append(local_run_metric)
+            all_local_test_metrics[run_folder_dir] += local_run_metric / NUM_CLIENTS
+
+            if eval_global_model:
+                server_model = load_global_model(run_folder_dir)
+                server_run_metric = evaluate_model(server_model, test_loader, metrics, device)
+                log(
+                    INFO,
+                    f"Client Number {client_number}, Run folder: {run_folder_dir}: "
+                    f"Server Model Test Performance: {server_run_metric}",
+                )
+                server_test_metrics.append(server_run_metric)
+                all_server_test_metrics[run_folder_dir] += server_run_metric / NUM_CLIENTS
+
+        avg_test_metric, std_test_metric = get_metric_avg_std(local_test_metrics)
+        log(INFO, f"Client {client_number} Model Average Test Performance on own Data: {avg_test_metric}")
+        log(INFO, f"Client {client_number} Model St. Dev. Test Performance on own Data: {std_test_metric}")
+        test_results[f"client_{client_number}_model_local_avg"] = avg_test_metric
+        test_results[f"client_{client_number}_model_local_std"] = std_test_metric
+
+        if eval_global_model:
+            avg_server_test_local_metric, std_server_test_local_metric = get_metric_avg_std(server_test_metrics)
+            log(INFO, f"Server model Average Test Performance on Client {client_number} Data: {avg_test_metric}")
+            log(INFO, f"Server model St. Dev. Test Performance on Client {client_number} Data: {std_test_metric}")
+            test_results[f"server_model_client_{client_number}_avg"] = avg_server_test_local_metric
+            test_results[f"server_model_client_{client_number}_std"] = std_server_test_local_metric
+
+    all_avg_test_metric, all_std_test_metric = get_metric_avg_std(list(all_local_test_metrics.values()))
+    test_results["avg_local_model_avg_across_clients"] = all_avg_test_metric
+    test_results["std_local_model_avg_across_clients"] = all_std_test_metric
+    log(INFO, f"Avg Local Model Test Performance Over all clients: {all_avg_test_metric}")
+    log(INFO, f"Std. Dev. Local Model Test Performance Over all clients: {all_std_test_metric}")
+
+    if eval_global_model:
+        all_server_avg_test_metric, all_server_std_test_metric = get_metric_avg_std(
+            list(all_server_test_metrics.values())
+        )
+        test_results["avg_server_model_avg_across_clients"] = all_server_avg_test_metric
+        test_results["std_server_model_avg_across_clients"] = all_server_std_test_metric
+        log(INFO, f"Avg. Server Model Test Performance Over all clients: {all_server_avg_test_metric}")
+        log(INFO, f"Std. Dev. Server Model Test Performance Over all clients: {all_server_std_test_metric}")
+
+    if eval_global_model:
+
+        # Next we test server checkpointed best model on pooled test data
+        pooled_test_dataset = FedIsic2019(center=0, train=False, pooled=True)
+        pooled_test_loader = DataLoader(pooled_test_dataset, batch_size=BATCH_SIZE, shuffle=False)
+
+        test_metrics = []
+        for run_folder_dir in all_run_folder_dir:
+            model = load_global_model(run_folder_dir)
+            run_metric = evaluate_model(model, pooled_test_loader, metrics, device)
+            log(INFO, f"Server, Run folder: {run_folder_dir}: Test Performance: {run_metric}")
+            test_metrics.append(run_metric)
+
+        avg_server_test_metric, std_server_test_metric = get_metric_avg_std(test_metrics)
+        log(INFO, f"Server Average Test Performance on Pooled Data: {avg_server_test_metric}")
+        log(INFO, f"Server St. Dev. Test Performance on Pooled Data: {std_server_test_metric}")
+        test_results["server_avg_pooled"] = avg_server_test_metric
+        test_results["server_std_pooled"] = std_server_test_metric
+
+    write_measurement_results(eval_write_path, test_results)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Evaluate Holdout Global")
+    parser.add_argument(
+        "--artifact_dir",
+        action="store",
+        type=str,
+        help="Path to saved model artifacts to be evaluated",
+        required=True,
+    )
+    parser.add_argument(
+        "--dataset_dir",
+        action="store",
+        type=str,
+        help="Path to the preprocessed FedIsic2019 Dataset (ex. path/to/fedisic2019)",
+        required=True,
+    )
+    parser.add_argument(
+        "--eval_write_path",
+        action="store",
+        type=str,
+        help="Path to write the evaluation results file",
+        required=True,
+    )
+    args = parser.parse_args()
+    parser.add_argument(
+        "--eval_global_model",
+        action="store_true",
+        type=str,
+        help="boolean to indicate whether to search for and evaluate a server model in addition to client models",
+        required=True,
+    )
+    log(INFO, f"Artifact Directory: {args.artifact_dir}")
+    log(INFO, f"Dataset Directory: {args.dataset_dir}")
+    log(INFO, f"Eval Write Path: {args.eval_write_path}")
+    log(INFO, f"Run Global Model: {args.eval_global_model}")
+    main(args.artifact_dir, args.dataset_dir, args.eval_write_path, args.eval_global_model)
