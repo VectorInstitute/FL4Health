886924ef3c3cb22f9a2eaa024b5ecaf6fa38a0c1
David Emerson <david.emerson@vectorinstitute.ai>
2023-07-04 16:36:24 -0400

    Hyper-parameter magnitude fix

--- research/flamby/fed_isic2019/fedadam/run_hp_sweep.sh
+++ research/flamby/fed_isic2019/fedadam/run_hp_sweep.sh
@@ -28,7 +28,7 @@ VENV_PATH=$4
 # FedISIC Server LR Hyperparmeters from paper are not suitable for AdamW
 SERVER_LR_VALUES=( 0.0001 0.001 0.01 0.1 1 )
 # FedISIC LR Hyperparmeters from paper are not suitable for AdamW
-LR_VALUES=( 0.0001 0.0001 0.001 0.01 0.1 )
+LR_VALUES=( 0.00001 0.0001 0.001 0.01 0.1 )
 
 SERVER_PORT=8100
 
--- research/flamby/fed_isic2019/fedprox/run_hp_sweep.sh
+++ research/flamby/fed_isic2019/fedprox/run_hp_sweep.sh
@@ -27,7 +27,7 @@ VENV_PATH=$4
 
 MU_VALUES=( 0.001 0.01 0.1 1.0 )
 # FedISIC LR Hyperparmeters from paper are not suitable for AdamW
-LR_VALUES=( 0.0001 0.0001 0.001 0.01 0.1 )
+LR_VALUES=( 0.00001 0.0001 0.001 0.01 0.1 )
 
 SERVER_PORT=8100
 
