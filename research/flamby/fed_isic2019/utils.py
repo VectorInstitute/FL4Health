ed2be54df3f7673e02098516c4905c3557ffc893
David Emerson <david.emerson@vectorinstitute.ai>
2023-07-05 15:46:00 -0400

    Killing model type assert to allow for models from APFL and FENDA to be evaluated.

--- research/flamby/fed_isic2019/utils.py
+++ research/flamby/fed_isic2019/utils.py
@@ -27,14 +27,12 @@ def write_measurement_results(eval_write_path: str, results: Dict[str, float]) -
 def load_local_model(run_folder_dir: str, client_number: int) -> Baseline:
     model_checkpoint_path = os.path.join(run_folder_dir, f"client_{client_number}_best_model.pkl")
     model = torch.load(model_checkpoint_path)
-    assert isinstance(model, Baseline)
     return model
 
 
 def load_global_model(run_folder_dir: str) -> Baseline:
     model_checkpoint_path = os.path.join(run_folder_dir, "server_best_model.pkl")
     model = torch.load(model_checkpoint_path)
-    assert isinstance(model, Baseline)
     return model
 
 
