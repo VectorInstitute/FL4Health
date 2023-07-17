aac5123811af72cab05700b23fc01cc62803de84
David Emerson <43939939+emersodb@users.noreply.github.com>
2023-07-16 20:05:45 -0400

    Adding in a fed heart disease config into the visualization processors.

--- research/flamby/visualization_scripts/average_performance_configs.py
+++ research/flamby/visualization_scripts/average_performance_configs.py
@@ -76,8 +76,213 @@ fed_isic_file_names_to_info: List[Tuple[str, str, Tuple[List[str], List[str]]]] 
         ),
     ),
     (
+        "fedadam_eval_performance.txt",
+        "FedAdam",
+        (
+            ["avg_server_model_avg_across_clients", "avg_local_model_avg_across_clients"],
+            ["std_server_model_avg_across_clients", "std_local_model_avg_across_clients"],
+        ),
+    ),
+    (
         "fenda_eval_performance_001.txt",
         "FENDA",
+        (
+            ["avg_local_model_avg_across_clients"],
+            ["std_local_model_avg_across_clients"],
+        ),
+    ),
+]
+
+# File name mapped to tuples of name appearing on the graph, keys for the mean, and keys for the std dev
+# NOTE: that only some methods with both server and local models have multiple mean and std dev keys
+fed_heart_disease_file_names_to_info: List[Tuple[str, str, Tuple[List[str], List[str]]]] = [
+    (
+        "central_eval_performance_small_model.txt",
+        "Central_S",
+        (
+            ["avg_server_model_avg_across_clients"],
+            ["std_server_model_avg_across_clients"],
+        ),
+    ),
+    (
+        "central_eval_performance_big_model.txt",
+        "Central_L",
+        (
+            ["avg_server_model_avg_across_clients"],
+            ["std_server_model_avg_across_clients"],
+        ),
+    ),
+    (
+        "client_0_eval_performance_small_model.txt",
+        "Local 0_S",
+        (
+            ["avg_server_model_avg_across_clients"],
+            ["std_server_model_avg_across_clients"],
+        ),
+    ),
+    (
+        "client_0_eval_performance_big_model.txt",
+        "Local 0_L",
+        (
+            ["avg_server_model_avg_across_clients"],
+            ["std_server_model_avg_across_clients"],
+        ),
+    ),
+    (
+        "client_1_eval_performance_small_model.txt",
+        "Local 1_S",
+        (
+            ["avg_server_model_avg_across_clients"],
+            ["std_server_model_avg_across_clients"],
+        ),
+    ),
+    (
+        "client_1_eval_performance_big_model.txt",
+        "Local 1_L",
+        (
+            ["avg_server_model_avg_across_clients"],
+            ["std_server_model_avg_across_clients"],
+        ),
+    ),
+    (
+        "client_2_eval_performance_small_model.txt",
+        "Local 2_S",
+        (
+            ["avg_server_model_avg_across_clients"],
+            ["std_server_model_avg_across_clients"],
+        ),
+    ),
+    (
+        "client_2_eval_performance_big_model.txt",
+        "Local 2_L",
+        (
+            ["avg_server_model_avg_across_clients"],
+            ["std_server_model_avg_across_clients"],
+        ),
+    ),
+    (
+        "client_3_eval_performance_small_model.txt",
+        "Local 3_S",
+        (
+            ["avg_server_model_avg_across_clients"],
+            ["std_server_model_avg_across_clients"],
+        ),
+    ),
+    (
+        "client_3_eval_performance_big_model.txt",
+        "Local 3_L",
+        (
+            ["avg_server_model_avg_across_clients"],
+            ["std_server_model_avg_across_clients"],
+        ),
+    ),
+    (
+        "client_4_eval_performance_small_model.txt",
+        "Local 4_S",
+        (
+            ["avg_server_model_avg_across_clients"],
+            ["std_server_model_avg_across_clients"],
+        ),
+    ),
+    (
+        "client_4_eval_performance_big_model.txt",
+        "Local 4_L",
+        (
+            ["avg_server_model_avg_across_clients"],
+            ["std_server_model_avg_across_clients"],
+        ),
+    ),
+    (
+        "client_5_eval_performance_small_model.txt",
+        "Local 5_S",
+        (
+            ["avg_server_model_avg_across_clients"],
+            ["std_server_model_avg_across_clients"],
+        ),
+    ),
+    (
+        "client_5_eval_performance_big_model.txt",
+        "Local 5_L",
+        (
+            ["avg_server_model_avg_across_clients"],
+            ["std_server_model_avg_across_clients"],
+        ),
+    ),
+    (
+        "fedprox_eval_performance_small_model.txt",
+        "FedProx_S",
+        (
+            ["avg_server_model_avg_across_clients", "avg_local_model_avg_across_clients"],
+            ["std_server_model_avg_across_clients", "std_local_model_avg_across_clients"],
+        ),
+    ),
+    (
+        "fedprox_eval_performance_big_model.txt",
+        "FedProx_L",
+        (
+            ["avg_server_model_avg_across_clients", "avg_local_model_avg_across_clients"],
+            ["std_server_model_avg_across_clients", "std_local_model_avg_across_clients"],
+        ),
+    ),
+    (
+        "fedavg_eval_performance_small_model.txt",
+        "FedAvg_S",
+        (
+            ["avg_server_model_avg_across_clients", "avg_local_model_avg_across_clients"],
+            ["std_server_model_avg_across_clients", "std_local_model_avg_across_clients"],
+        ),
+    ),
+    (
+        "fedavg_eval_performance_big_model.txt",
+        "FedAvg_L",
+        (
+            ["avg_server_model_avg_across_clients", "avg_local_model_avg_across_clients"],
+            ["std_server_model_avg_across_clients", "std_local_model_avg_across_clients"],
+        ),
+    ),
+    (
+        "fedadam_eval_performance_small_model.txt",
+        "FedAdam_S",
+        (
+            ["avg_server_model_avg_across_clients", "avg_local_model_avg_across_clients"],
+            ["std_server_model_avg_across_clients", "std_local_model_avg_across_clients"],
+        ),
+    ),
+    (
+        "fedadam_eval_performance_big_model.txt",
+        "FedAdam_L",
+        (
+            ["avg_server_model_avg_across_clients", "avg_local_model_avg_across_clients"],
+            ["std_server_model_avg_across_clients", "std_local_model_avg_across_clients"],
+        ),
+    ),
+    (
+        "scaffold_eval_performance_small_model.txt",
+        "SCAFFOLD_S",
+        (
+            ["avg_server_model_avg_across_clients", "avg_local_model_avg_across_clients"],
+            ["std_server_model_avg_across_clients", "std_local_model_avg_across_clients"],
+        ),
+    ),
+    (
+        "scaffold_eval_performance_big_model.txt",
+        "SCAFFOLD_L",
+        (
+            ["avg_server_model_avg_across_clients", "avg_local_model_avg_across_clients"],
+            ["std_server_model_avg_across_clients", "std_local_model_avg_across_clients"],
+        ),
+    ),
+    (
+        "fenda_eval_performance_big_model.txt",
+        "FENDA",
+        (
+            ["avg_local_model_avg_across_clients"],
+            ["std_local_model_avg_across_clients"],
+        ),
+    ),
+    (
+        "apfl_eval_performance_big_model.txt",
+        "APFL",
         (
             ["avg_local_model_avg_across_clients"],
             ["std_local_model_avg_across_clients"],
