4acd0bb59644e9148522339bbd4b2864082dbfb1
David Emerson <43939939+emersodb@users.noreply.github.com>
2023-08-07 08:21:50 -0400

    Fixing another issue with Monai tensors not being on the same device

--- research/flamby/flamby_data_utils.py
+++ research/flamby/flamby_data_utils.py
@@ -3,6 +3,7 @@ from typing import Tuple
 from flamby.datasets.fed_heart_disease import FedHeartDisease
 from flamby.datasets.fed_isic2019 import FedIsic2019
 from flamby.datasets.fed_ixi import FedIXITiny
+from monai.transforms import ToTensor
 from torch.utils.data import random_split
 
 
@@ -25,7 +26,9 @@ def construct_fed_heard_disease_train_val_datasets(
 
 
 def construct_fed_ixi_train_val_datasets(client_number: int, dataset_dir: str) -> Tuple[FedIXITiny, FedIXITiny]:
-    full_train_dataset = FedIXITiny(center=client_number, train=True, pooled=False, data_path=dataset_dir)
+    full_train_dataset = FedIXITiny(
+        center=client_number, train=True, pooled=False, data_path=dataset_dir, transform=ToTensor(track_meta=False)
+    )
     # Something weird is happening with the typing of the split sequence in random split. Punting with a mypy
     # ignore for now.
     train_dataset, validation_dataset = tuple(random_split(full_train_dataset, [0.8, 0.2]))  # type: ignore
