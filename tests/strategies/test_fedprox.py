f0bcf018ddf7fc915bff99b73b891ad428abb5f8
sanaAyrml <ayromlous@gmail.com>
2023-08-23 08:35:47 -0700

    Edit strategy test

--- tests/strategies/test_fedprox.py
+++ tests/strategies/test_fedprox.py
@@ -31,11 +31,13 @@ def test_maybe_update_proximal_weight_param_decrease() -> None:
     strat = FedProx(initial_parameters=params, adaptive_proximal_weight=True, proximal_weight=initial_proximal_weight)
     strat.previous_loss = initial_loss
 
-    for _ in range(strat.proximal_weight_patience):
+    for c in range(strat.proximal_weight_patience):
         pre_proximal_weight_patience_counter = strat.proximal_weight_patience_counter
         new_loss: float = strat.previous_loss - 1
         strat._maybe_update_proximal_weight_param(new_loss)
-        assert strat.proximal_weight_patience_counter == pre_proximal_weight_patience_counter + 1
-
-    assert strat.proximal_weight == initial_proximal_weight - strat.proximal_weight_delta
-    assert strat.proximal_weight_patience_counter == 0
+        if c == strat.proximal_weight_patience - 1:
+            assert strat.proximal_weight == initial_proximal_weight - strat.proximal_weight_delta
+            assert strat.proximal_weight_patience_counter == 0
+        else:
+            assert strat.proximal_weight == initial_proximal_weight
+            assert strat.proximal_weight_patience_counter == pre_proximal_weight_patience_counter + 1
