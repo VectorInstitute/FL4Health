a0fa16369b84b106bcf811768058c8c4a952984e
sanaAyrml <ayromlous@gmail.com>
2023-11-08 18:37:37 -0800

    Add apfl warmup

--- examples/apfl_example/fedavg_warm_up/client.py
+++ examples/apfl_example/fedavg_warm_up/client.py
@@ -1,7 +1,7 @@
 import argparse
 from logging import INFO
 from pathlib import Path
-from typing import Dict, Tuple
+from typing import Tuple
 
 import flwr as fl
 import torch
@@ -13,14 +13,14 @@ from torch.optim import Optimizer
 from torch.utils.data import DataLoader
 
 from examples.models.cnn_model import MnistNetWithBnAndFrozen
-from fl4health.clients.apfl_client import ApflClient
+from fl4health.clients.basic_client import BasicClient
 from fl4health.model_bases.apfl_base import ApflModule
 from fl4health.utils.load_data import load_mnist_data
 from fl4health.utils.metrics import Accuracy
 from fl4health.utils.sampler import DirichletLabelBasedSampler
 
 
-class MnistApflClient(ApflClient):
+class WarmUpMnistClient(BasicClient):
     def get_data_loaders(self, config: Config) -> Tuple[DataLoader, DataLoader]:
         batch_size = self.narrow_config_type(config, "batch_size", int)
         sampler = DirichletLabelBasedSampler(list(range(10)), sample_percentage=0.75)
@@ -28,12 +28,12 @@ class MnistApflClient(ApflClient):
         return train_loader, val_loader
 
     def get_model(self, config: Config) -> nn.Module:
-        return ApflModule(MnistNetWithBnAndFrozen()).to(self.device)
+        return ApflModule(MnistNetWithBnAndFrozen(), warm_up=True).to(self.device)
 
-    def get_optimizer(self, config: Config) -> Dict[str, Optimizer]:
-        local_optimizer = torch.optim.AdamW(self.model.local_model.parameters(), lr=0.01)
-        global_optimizer = torch.optim.AdamW(self.model.global_model.parameters(), lr=0.01)
-        return {"local": local_optimizer, "global": global_optimizer}
+    def get_optimizer(self, config: Config) -> Optimizer:
+        if isinstance(self.model.global_model, torch.nn.Module):
+            global_optimizer = torch.optim.AdamW(self.model.global_model.parameters(), lr=0.01)
+        return global_optimizer
 
     def get_criterion(self, config: Config) -> _Loss:
         return torch.nn.CrossEntropyLoss()
@@ -63,7 +63,7 @@ if __name__ == "__main__":
     log(INFO, f"Device to be used: {DEVICE}")
     log(INFO, f"Server Address: {args.server_address}")
 
-    client = MnistApflClient(data_path, [Accuracy()], DEVICE, seed=args.seed)
+    client = WarmUpMnistClient(data_path, [Accuracy()], DEVICE, seed=args.seed)
     fl.client.start_numpy_client(server_address=args.server_address, client=client)
 
     # Shutdown the client gracefully
