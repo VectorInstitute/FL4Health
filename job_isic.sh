4191b1debfb7f12f4e455b6c8d2f395d64a0fba9
sanaAyrml <ayromlous@gmail.com>
2023-12-04 13:37:18 -0500

    server changes commit

--- job_isic.sh
+++ job_isic.sh
@@ -32,7 +32,20 @@ source /ssd003/projects/aieng/public/FL_env/env3/bin/activate
 
 # python3 -m research.flamby.fed_isic2019.evaluate_on_holdout --artifact_dir /ssd003/projects/aieng/public/FL_env/models/fed_isic2019/fenda/hp_sweep_results_perFCL_pre_trained/lr_0.001/ --dataset_dir /ssd003/projects/aieng/public/flamby_datasets/fed_isic2019/ --eval_write_path ~/FL/FL4Health/research/flamby/fed_isic2019/fenda/test_eval_performance_perFCL_pre_trained.txt --eval_local_models
 
-python3 -m research.flamby.fed_isic2019.evaluate_on_holdout --artifact_dir /ssd003/projects/aieng/public/FL_env/models/fed_isic2019/fenda/hp_sweep_results_new/lr_0.001/ --dataset_dir /ssd003/projects/aieng/public/flamby_datasets/fed_isic2019/ --eval_write_path ~/FL/FL4Health/research/flamby/fed_isic2019/fenda/test_eval_performance_new.txt --eval_local_models
+# python3 -m research.flamby.fed_isic2019.evaluate_on_holdout --artifact_dir /ssd003/projects/aieng/public/FL_env/models/fed_isic2019/fenda/hp_sweep_results_new/lr_0.001/ --dataset_dir /ssd003/projects/aieng/public/flamby_datasets/fed_isic2019/ --eval_write_path ~/FL/FL4Health/research/flamby/fed_isic2019/fenda/test_eval_performance_new.txt --eval_local_models
+
+
+# python3 -m research.flamby.fed_isic2019.evaluate_on_holdout --artifact_dir /ssd003/projects/aieng/public/FL_env/models/fed_isic2019/fenda/hp_sweep_results_cos_sim_pre_trained_double/lr_0.001/ --dataset_dir /ssd003/projects/aieng/public/flamby_datasets/fed_isic2019/ --eval_write_path ~/FL/FL4Health/research/flamby/fed_isic2019/fenda/test_eval_performance_cos_sim_pre_trained_double.txt --eval_local_models
+
+# python3 -m research.flamby.fed_isic2019.evaluate_on_holdout --artifact_dir /ssd003/projects/aieng/public/FL_env/models/fed_isic2019/fenda/hp_sweep_results_contrastive_pre_trained_double/lr_0.001/ --dataset_dir /ssd003/projects/aieng/public/flamby_datasets/fed_isic2019/ --eval_write_path ~/FL/FL4Health/research/flamby/fed_isic2019/fenda/test_eval_performance_contrastive_pre_trained_double.txt --eval_local_models
+
+# python3 -m research.flamby.fed_isic2019.evaluate_on_holdout --artifact_dir /ssd003/projects/aieng/public/FL_env/models/fed_isic2019/fenda/hp_sweep_results_perFCL_pre_trained_double/lr_0.001/ --dataset_dir /ssd003/projects/aieng/public/flamby_datasets/fed_isic2019/ --eval_write_path ~/FL/FL4Health/research/flamby/fed_isic2019/fenda/test_eval_performance_perFCL_pre_trained_double.txt --eval_local_models
+
+# python3 -m research.flamby.fed_isic2019.evaluate_on_holdout --artifact_dir /ssd003/projects/aieng/public/FL_env/models/fed_isic2019/fenda/hp_sweep_results_pre_trained_double/lr_0.001/ --dataset_dir /ssd003/projects/aieng/public/flamby_datasets/fed_isic2019/ --eval_write_path ~/FL/FL4Health/research/flamby/fed_isic2019/fenda/test_eval_performance_pre_trained_double.txt --eval_local_models
+
+# python3 -m research.flamby.fed_isic2019.evaluate_on_holdout --artifact_dir /ssd003/projects/aieng/public/FL_env/models/fed_isic2019/moon/hp_sweep_results_new/lr_0.001/ --dataset_dir /ssd003/projects/aieng/public/flamby_datasets/fed_isic2019/ --eval_write_path ~/FL/FL4Health/research/flamby/fed_isic2019/moon/test_eval_performance_new.txt --eval_local_models
+
+python3 -m research.flamby.fed_isic2019.evaluate_on_holdout --artifact_dir /ssd003/projects/aieng/public/FL_env/models/fed_isic2019/moon/hp_sweep_results_vanilla/lr_0.001/ --dataset_dir /ssd003/projects/aieng/public/flamby_datasets/fed_isic2019/ --eval_write_path ~/FL/FL4Health/research/flamby/fed_isic2019/moon/test_eval_performance_vanilla.txt --eval_local_models
 
 # python3 -m research.flamby.fed_isic2019.evaluate_on_holdout --artifact_dir /ssd003/projects/aieng/public/FL_env/models/fed_isic2019/fenda/hp_sweep_results_perFCL/lr_0.001/ --dataset_dir /ssd003/projects/aieng/public/flamby_datasets/fed_isic2019/ --eval_write_path ~/FL/FL4Health/research/flamby/fed_isic2019/fenda/test_eval_performance_perFCL.txt --eval_local_models
 
