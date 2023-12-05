24ba1b2977c959310f99814fc502c67fc85075f8
Marcelo Lotif <lotif@users.noreply.github.com>
2023-12-05 16:42:38 -0500

    Update run_smoke_test.py

--- tests/smoke_tests/run_smoke_test.py
+++ tests/smoke_tests/run_smoke_test.py
@@ -11,7 +11,7 @@ from six.moves import urllib
 from examples.fedprox_example.client import MnistFedProxClient
 from fl4health.utils.metrics import Accuracy
 
-logging.basicConfig(format="%(asctime)s %(levelname)-8s %(message)s", level=logging.DEBUG, datefmt="%Y-%m-%d %H:%M:%S")
+logging.basicConfig(format="%(asctime)s %(levelname)-8s %(message)s", level=logging.INFO, datefmt="%Y-%m-%d %H:%M:%S")
 logger = logging.getLogger()
 
 
