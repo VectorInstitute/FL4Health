2dc4ecbfb4ffe60fae00c991e9d75a84dd3e9aef
sanaAyrml <ayromlous@gmail.com>
2023-12-12 18:25:27 -0800

    Add fenda fedisic

--- research/flamby/fed_isic2019/fenda/client.py
+++ research/flamby/fed_isic2019/fenda/client.py
@@ -14,7 +14,11 @@ from torch.nn.modules.loss import _Loss
 from torch.optim import Optimizer
 from torch.utils.data import DataLoader
 
-from fl4health.checkpointing.checkpointer import BestMetricTorchCheckpointer, TorchCheckpointer
+from fl4health.checkpointing.checkpointer import (
+    BestMetricTorchCheckpointer,
+    LatestTorchCheckpointer,
+    TorchCheckpointer,
+)
 from fl4health.clients.fenda_client import FendaClient
 from fl4health.utils.losses import LossMeterType
 from fl4health.utils.metrics import BalancedAccuracy, Metric
@@ -34,7 +38,8 @@ class FedIsic2019FendaClient(FendaClient):
         checkpointer: Optional[TorchCheckpointer] = None,
         cos_sim_activate: bool = False,
         contrastive_activate: bool = False,
-        perfcl_activate: bool = False,
+        extra_loss_weights: Optional[float] = None,
+        seed: Optional[int] = None,
     ) -> None:
         super().__init__(
             data_path=data_path,
@@ -42,15 +47,16 @@ class FedIsic2019FendaClient(FendaClient):
             device=device,
             loss_meter_type=loss_meter_type,
             checkpointer=checkpointer,
+            seed=seed,
         )
         self.client_number = client_number
         self.learning_rate = learning_rate
         if cos_sim_activate:
-            self.cos_sim_loss_weight = 100.0
+            assert isinstance(extra_loss_weights, float)
+            self.cos_sim_loss_weight = extra_loss_weights
         if contrastive_activate:
-            self.contrastive_loss_weight = 10.0
-        if perfcl_activate:
-            self.perfcl_loss_weights = (10.0, 10.0)
+            assert isinstance(extra_loss_weights, float)
+            self.contrastive_loss_weight = extra_loss_weights
 
         assert 0 <= client_number < NUM_CLIENTS
         log(INFO, f"Client Name: {self.client_name}, Client Number: {self.client_number}")
@@ -113,19 +119,43 @@ if __name__ == "__main__":
     parser.add_argument(
         "--learning_rate", action="store", type=float, help="Learning rate for local optimization", default=LR
     )
+    parser.add_argument(
+        "--seed",
+        action="store",
+        type=int,
+        help="Seed for the random number generator",
+        required=False,
+    )
     parser.add_argument("--cos_sim_loss", action="store_true", help="Activate Cosine Similarity loss")
     parser.add_argument("--contrastive_loss", action="store_true", help="Activate Contrastive loss")
-    parser.add_argument("--perfcl_loss", action="store_true", help="Activate PerFCL loss")
+    parser.add_argument(
+        "--mu",
+        action="store",
+        type=float,
+        help="Weight for the auxiliary losses",
+        required=False,
+    )
+    parser.add_argument(
+        "--no_federated_checkpointing",
+        action="store_true",
+        help="boolean to disable client-side federated checkpointing in the personal FL experiment",
+    )
     args = parser.parse_args()
 
     DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     log(INFO, f"Device to be used: {DEVICE}")
     log(INFO, f"Server Address: {args.server_address}")
     log(INFO, f"Learning Rate: {args.learning_rate}")
+    log(INFO, f"Performing Federated Checkpointing: {not args.no_federated_checkpointing}")
 
+    federated_checkpointing = not args.no_federated_checkpointing
     checkpoint_dir = os.path.join(args.artifact_dir, args.run_name)
     checkpoint_name = f"client_{args.client_number}_best_model.pkl"
-    checkpointer = BestMetricTorchCheckpointer(checkpoint_dir, checkpoint_name, maximize=False)
+    checkpointer = (
+        BestMetricTorchCheckpointer(checkpoint_dir, checkpoint_name, maximize=False)
+        if federated_checkpointing
+        else LatestTorchCheckpointer(checkpoint_dir, checkpoint_name)
+    )
 
     client = FedIsic2019FendaClient(
         data_path=Path(args.dataset_dir),
@@ -136,7 +166,8 @@ if __name__ == "__main__":
         checkpointer=checkpointer,
         cos_sim_activate=args.cos_sim_loss,
         contrastive_activate=args.contrastive_loss,
-        perfcl_activate=args.perfcl_loss,
+        seed=args.seed,
+        extra_loss_weights=args.mu,
     )
 
     fl.client.start_numpy_client(server_address=args.server_address, client=client)
