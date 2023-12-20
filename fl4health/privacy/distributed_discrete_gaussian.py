dd3dccd32a76e9a7744d3a605a44bc5d919fb06c
zxj-c <143853024+zxj-c@users.noreply.github.com>
2023-12-20 13:16:22 -0500

    Search over alpha and delta

--- fl4health/privacy/distributed_discrete_gaussian.py
+++ fl4health/privacy/distributed_discrete_gaussian.py
@@ -51,5 +51,37 @@ class DiscreteGaussianPrivacy:
 
         return min([b1, b2, b3])
 
+    def epsilon_delta(self, delta: float) -> float:
+        """Returns the epsilon, for any 0 < delta < 1
+        Based on Proposition 3 of https://arxiv.org/pdf/1702.07476.pdf
+        """
+        assert 0 < delta < 1
+        epsilon = self.alpha + math.log(1 / delta) / (self.alpha - 1)
+        return epsilon
+
+    def optimize_epsilon(self, exponent: int) -> float:
+        """Optimize over all delta"""
+        search_scale = 1 / 2**exponent
+        min_eps: float = 10
+        for step in range(0, 2**exponent):
+            eps = self.epsilon_delta(step * search_scale)
+            if eps < min_eps:
+                min_eps = eps
+        return min_eps
+
+    def optimize_alpha(self, exponent: int, alpha_upper_bound: float = 200) -> float:
+        """Optimize over all alpha, use same exponent for epsilon search as well."""
+        search_scale = 1 / 2**exponent
+        min_eps: float = 10
+
+        self.alpha = 1 + search_scale
+        while self.alpha <= alpha_upper_bound:
+            eps = self.optimize_epsilon(exponent)
+            if eps < min_eps:
+                min_eps = eps
+            self.alpha += search_scale
+
+        return min_eps
+
 
 # TODO create tests
