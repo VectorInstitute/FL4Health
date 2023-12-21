c62b536662e03ffed6320cdb372d9c3848499ad7
Marcelo Lotif <marcelo.lotif@vectorinstitute.ai>
2023-12-21 11:53:30 -0500

    bump [2]

--- tests/smoke_tests/run_smoke_test.py
+++ tests/smoke_tests/run_smoke_test.py
@@ -13,7 +13,7 @@ from examples.fedprox_example.client import MnistFedProxClient
 from fl4health.utils.load_data import load_cifar10_data
 from fl4health.utils.metrics import Accuracy
 
-logging.basicConfig(format="%(asctime)s %(levelname)-8s %(message)s", level=logging.DEBUG, datefmt="%Y-%m-%d %H:%M:%S")
+logging.basicConfig(format="%(asctime)s %(levelname)-8s %(message)s", level=logging.INFO, datefmt="%Y-%m-%d %H:%M:%S")
 logger = logging.getLogger()
 
 
