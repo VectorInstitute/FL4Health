680281165e37bbc8cae6e716c6bd39a0130ab3e7
sanaAyrml <ayromlous@gmail.com>
2024-03-25 10:21:13 -0400

    change range

--- fl4health/losses/mkmmd_loss.py
+++ fl4health/losses/mkmmd_loss.py
@@ -37,8 +37,8 @@ class MkMmdLoss(torch.nn.Module):
         super().__init__()
         self.device = device
         if gammas is None:
-            # Note arange is not inclusive, so this ends up being [-3.5, 3.5] in steps of 0.5
-            default_gamma_powers = torch.arange(-3.5, 3.75, 0.25, device=device)
+            # Note arange is not inclusive, so this ends up being [-3.5, 1] in steps of 0.5
+            default_gamma_powers = torch.arange(-3.5, 1,25, 0.25, device=device)
             self.gammas = torch.pow(2.0, default_gamma_powers)
         else:
             self.gammas = gammas
