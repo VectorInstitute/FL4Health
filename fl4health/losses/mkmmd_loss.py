2a53c5828e4d9c657c8425336c3ebc997f400a83
sanaAyrml <ayromlous@gmail.com>
2024-03-25 10:19:21 -0400

    solve infeasibility issue

--- fl4health/losses/mkmmd_loss.py
+++ fl4health/losses/mkmmd_loss.py
@@ -261,7 +261,12 @@ class MkMmdLoss(torch.nn.Module):
             )
 
         if self.minimize_type_two_error:
-            unnormalized_betas = self.form_and_solve_qp(hat_d_per_kernel, regularized_Q_k)
+            try:
+                unnormalized_betas = self.form_and_solve_qp(hat_d_per_kernel, regularized_Q_k)
+            except Exception as e:
+                # If we can't solve the QP due to infeasibility, then we keep the previous betas
+                log(INFO, f"{e} We keep previous betas.")
+                unnormalized_betas = self.betas
         else:
             # If we're trying to maximize the type II error, then we are trying to maximize a convex function over a
             # convex polygon of beta values. So the maximum is found at one of the vertices
