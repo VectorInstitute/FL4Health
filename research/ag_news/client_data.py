a38f8ec16006de50b5f6cae9d9177539d3f87658
Yuchong Zhang <yuchong.zhang@vectorinstitute.ai>
2024-03-27 15:57:20 -0400

    allowing for passing in data file

--- research/ag_news/client_data.py
+++ research/ag_news/client_data.py
@@ -1,5 +1,6 @@
 from functools import partial
-from typing import Dict, List, Tuple
+from pathlib import Path
+from typing import Dict, List, Optional, Tuple
 
 import datasets
 import torch
@@ -34,12 +35,17 @@ def create_text_classification_dataset(
     return DictionaryDataset(data=data_dict, targets=targets)
 
 
-def construct_dataloaders(batch_size: int, sample_percentage: float, beta: float) -> Tuple[DataLoader, DataLoader]:
+def construct_dataloaders(
+    batch_size: int, sample_percentage: float, beta: float, data_path: Optional[Path] = None
+) -> Tuple[DataLoader, DataLoader]:
 
     assert 0 <= sample_percentage <= 1 and beta > 0
     sampler = DirichletLabelBasedSampler(list(range(4)), sample_percentage=sample_percentage, beta=beta)
 
-    ag_news_dataset = load_dataset("ag_news")
+    if data_path is None:
+        ag_news_dataset = load_dataset("ag_news")
+    else:
+        ag_news_dataset = load_dataset(name="ag_news", data_dir=data_path)
     bert_tokenizer = BertTokenizer.from_pretrained("google-bert/bert-base-cased")
     # Apply Bert's tokenizer to all data points. Also perform truncation.
     tokenized_ag_news = ag_news_dataset.map(
