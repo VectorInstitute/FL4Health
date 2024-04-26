afd4c0b7b48d90e8e1be3f8ac952484330c4833e
David Emerson <43939939+emersodb@users.noreply.github.com>
2024-04-26 09:17:47 -0400

    Adding in a bit of documentation to the helper functions.

--- fl4health/utils/parameter_extraction.py
+++ fl4health/utils/parameter_extraction.py
@@ -4,5 +4,15 @@ from flwr.common.typing import Parameters
 
 
 def get_all_model_parameters(model: nn.Module) -> Parameters:
+    """
+    Function to extract ALL parameters associated with a pytorch module, including any state parameters. These
+    values are converted from numpy arrays into a Flower Parameters object.
+
+    Args:
+        model (nn.Module): PyTorch model whose parameters are to be extracted
+
+    Returns:
+        Parameters: Flower Parameters object containing all of the target models state.
+    """
     # Extracting all model parameters and converting to Parameters object
     return ndarrays_to_parameters([val.cpu().numpy() for _, val in model.state_dict().items()])
--- fl4health/utils/privacy_utilities.py
+++ fl4health/utils/privacy_utilities.py
@@ -9,9 +9,22 @@ from opacus.validators import ModuleValidator
 
 
 def privacy_validate_and_fix_modules(model: nn.Module) -> Tuple[nn.Module, bool]:
-    # Validate that the model layers are compatible with privacy mechanisms in Opacus and try to replace the layers
-    # with compatible ones if necessary.
+    """
+    This function runs Opacus model validation to ensure that the provided models layers are compatible with the
+    privacy mechanisms in Opacus. The function attempts to use Opacus to replace any incompatible layers if possible.
+    For example BatchNormalization layers are not "DP Compliant" and will be replaced by compliant layers such as
+    GroupNormalization with this function. Note that this uses the default "fix" functionality in Opacus. For more
+    custom options, defining your own setup_opacus_objects function is required.
 
+    Args:
+        model (nn.Module): The model to be validated and potentially modified to be Opacus compliant.
+
+    Returns:
+        Tuple[nn.Module, bool]: Returns a (possibly) modified pytorch model and a boolean indicating whether a
+            reinitialization of any optimizers associated with the model will be required. Reinitialization of the
+            optimizer parameters is required, for example, when the model layers are modified, yielding a mismatch
+            in the optimizer parameters and the new model parameters.
+    """
     errors = ModuleValidator.validate(model, strict=False)
     reinitialize_optimizer = len(errors) > 0
     # Due to a bug in Opacus, it's possible that we need to run multiple rounds fo module validator fix for
@@ -34,6 +47,19 @@ def privacy_validate_and_fix_modules(model: nn.Module) -> Tuple[nn.Module, bool]
 def convert_model_to_opacus_model(
     model: nn.Module, grad_sample_mode: str = "hooks", *args: Any, **kwargs: Any
 ) -> GradSampleModule:
+    """
+    This function converts a standard pytorch model to an Opacus GradSampleModule, which Opacus uses to perform
+    efficient DP-SGD operations. It uses the wrap_model functionality and mimics its defaults.
+
+    Args:
+        model (nn.Module): Pytorch model to be converted to an Opacus GradSampleModule
+        grad_sample_mode (str, optional): This determines how Opacus performs the conversion under the hood. The
+            standard mechanism is indicated by "hooks" but other approaches may be necessary depending on how the
+            pytorch module is defined. Defaults to "hooks".
+
+    Returns:
+        GradSampleModule: The Opacus wrapped GradSampleModule
+    """
     if isinstance(model, GradSampleModule):
         log(INFO, f"Provided model is already of type {type(model)}, skipping conversion to Opacus model type")
         return model
@@ -43,5 +69,19 @@ def convert_model_to_opacus_model(
 def map_model_to_opacus_model(
     model: nn.Module, grad_sample_mode: str = "hooks", *args: Any, **kwargs: Any
 ) -> GradSampleModule:
+    """
+    Performs an validation and modifications necessary to make the provided pytorch model "Opacus Compliant" via the
+    call to privacy_validate_and_fix_modules. The resulting model is then converted to an Opacus GradSampleModule via
+    convert_model_to_opacus_model.
+
+    Args:
+        model (nn.Module): Pytorch model to be converted to an Opacus compliant GradSampleModule
+        grad_sample_mode (str, optional): This determines how Opacus performs the conversion under the hood. The
+            standard mechanism is indicated by "hooks" but other approaches may be necessary depending on how the
+            pytorch module is defined. Defaults to "hooks".
+
+    Returns:
+        GradSampleModule: The Opacus-compliant, wrapped GradSampleModule
+    """
     model, _ = privacy_validate_and_fix_modules(model)
     return convert_model_to_opacus_model(model, grad_sample_mode, *args, **kwargs)
