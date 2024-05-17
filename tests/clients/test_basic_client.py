2f9859421f704c1eb728b77a70dc9a3f91761ae5
Justin <yilin.yang@vectorinstitute.ai>
2024-05-16 20:53:20 -0700

    minor test error fix

--- tests/clients/test_basic_client.py
+++ tests/clients/test_basic_client.py
@@ -62,7 +62,7 @@ def test_metrics_reporter_evaluate() -> None:
     test_loss = 123.123
     test_metrics: Dict[str, Scalar] = {"test_metric": 1234}
     test_metrics_testing: Dict[str, Scalar] = {"testing_metric": 1234}
-    test_metrics: Dict[str, Scalar] = {"test_metric": 1234, "testing_metric": 1234, "test - loss": 123.123}
+    test_metrics = {"test_metric": 1234, "testing_metric": 1234, "test - loss": 123.123}
 
     fl_client = MockBasicClient(loss=test_loss, metrics=test_metrics, test_set_metrics=test_metrics_testing)
     fl_client.evaluate([], {"current_server_round": test_current_server_round, "local_epochs": 0})
