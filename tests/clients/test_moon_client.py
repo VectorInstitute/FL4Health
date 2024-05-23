9ff76cd0211c537a27812b7837fc174d7b9fb483
David Emerson <43939939+emersodb@users.noreply.github.com>
2024-05-23 10:43:36 -0400

    Adding test to ensure that the moon client properly pops models when the queue fills.

--- tests/clients/test_moon_client.py
+++ tests/clients/test_moon_client.py
@@ -16,12 +16,12 @@ def test_setting_parameters(get_client: MoonClient) -> None:  # noqa
     moon_client = get_client
     config: Config = {"current_server_round": 1}
 
-    params = [copy.deepcopy(val.cpu().numpy()) for _, val in moon_client.model.state_dict().items()]
+    params = [copy.deepcopy(val.cpu().numpy()) for val in moon_client.model.state_dict().values()]
     new_params = [layer_weights + 0.1 for layer_weights in params]
     moon_client.set_parameters(new_params, config, fitting_round=True)
 
     # Make sure the MOON model parameters are equal to the global model parameters
-    new_moon_model_params = [val.cpu().numpy() for _, val in moon_client.model.state_dict().items()]
+    new_moon_model_params = [val.cpu().numpy() for val in moon_client.model.state_dict().values()]
 
     for i in range(len(params)):
         assert (new_params[i] == new_moon_model_params[i]).all()
@@ -39,12 +39,12 @@ def test_setting_global_model(get_client: MoonClient) -> None:  # noqa
 
     assert moon_client.global_model is None
 
-    params = [copy.deepcopy(val.cpu().numpy()) for _, val in moon_client.model.state_dict().items()]
+    params = [copy.deepcopy(val.cpu().numpy()) for val in moon_client.model.state_dict().values()]
     moon_client.update_before_train(0)
 
     assert moon_client.global_model is not None
 
-    global_params = [copy.deepcopy(val.cpu().numpy()) for _, val in moon_client.global_model.state_dict().items()]
+    global_params = [copy.deepcopy(val.cpu().numpy()) for val in moon_client.global_model.state_dict().values()]
     # Make sure the MOON model parameters are equal to the global model parameters
     for i in range(len(params)):
         assert (params[i] == global_params[i]).all()
@@ -69,7 +69,7 @@ def test_setting_old_models(get_client: MoonClient) -> None:  # noqa
 
     assert len(moon_client.old_models_list) == 0
 
-    params = [copy.deepcopy(val.cpu().numpy()) for _, val in moon_client.model.state_dict().items()]
+    params = [copy.deepcopy(val.cpu().numpy()) for val in moon_client.model.state_dict().values()]
     loss = {
         "loss": 0.0,
     }
@@ -79,7 +79,7 @@ def test_setting_old_models(get_client: MoonClient) -> None:  # noqa
     assert len(moon_client.old_models_list) == 1
 
     old_model_params = [
-        copy.deepcopy(val.cpu().numpy()) for _, val in moon_client.old_models_list[0].state_dict().items()
+        copy.deepcopy(val.cpu().numpy()) for val in moon_client.old_models_list[0].state_dict().values()
     ]
     # Make sure the MOON model parameters are equal to the old model parameters
     for i in range(len(params)):
@@ -106,7 +106,7 @@ def test_getting_parameters(get_client: MoonClient) -> None:  # noqa
 
     assert len(moon_client.old_models_list) == 0
 
-    params = [copy.deepcopy(val.cpu().numpy()) for _, val in moon_client.model.state_dict().items()]
+    params = [copy.deepcopy(val.cpu().numpy()) for val in moon_client.model.state_dict().values()]
     loss = {
         "loss": 0.0,
     }
@@ -124,10 +124,10 @@ def test_getting_parameters(get_client: MoonClient) -> None:  # noqa
     assert len(moon_client.old_models_list) == 1
     assert moon_client.global_model is not None
 
-    old_model_params = [val.cpu().numpy() for _, val in moon_client.old_models_list[-1].state_dict().items()]
-    global_model_params = [val.cpu().numpy() for _, val in moon_client.global_model.state_dict().items()]
+    old_model_params = [val.cpu().numpy() for val in moon_client.old_models_list[-1].state_dict().values()]
+    global_model_params = [val.cpu().numpy() for val in moon_client.global_model.state_dict().values()]
     # Make sure the MOON model parameters are equal to the global model parameters
-    new_moon_model_params = [val.cpu().numpy() for _, val in moon_client.model.state_dict().items()]
+    new_moon_model_params = [val.cpu().numpy() for val in moon_client.model.state_dict().values()]
 
     for i in range(len(params)):
         assert (params[i] == old_model_params[i]).all()
@@ -149,10 +149,10 @@ def test_getting_parameters(get_client: MoonClient) -> None:  # noqa
     # Assert we stored the old model
     assert len(moon_client.old_models_list) == 1
 
-    old_model_params = [val.cpu().numpy() for _, val in moon_client.old_models_list[-1].state_dict().items()]
-    global_model_params = [val.cpu().numpy() for _, val in moon_client.global_model.state_dict().items()]
+    old_model_params = [val.cpu().numpy() for val in moon_client.old_models_list[-1].state_dict().values()]
+    global_model_params = [val.cpu().numpy() for val in moon_client.global_model.state_dict().values()]
     # Make sure the MOON model parameters are equal to the global model parameters
-    new_moon_model_params = [val.cpu().numpy() for _, val in moon_client.model.state_dict().items()]
+    new_moon_model_params = [val.cpu().numpy() for val in moon_client.model.state_dict().values()]
 
     for i in range(len(params)):
         assert (new_params[i] == old_model_params[i]).all()
@@ -265,5 +265,45 @@ def test_computing_first_loss(get_client: MoonClient) -> None:  # noqa
     assert pytest.approx(expected_loss, abs=0.0001) == evaluation_loss.checkpoint.item()
     assert pytest.approx(expected_total_loss, abs=0.0001) == training_loss.backward["backward"].item()
     assert training_loss.additional_losses == evaluation_loss.additional_losses
+
+    torch.seed()  # resetting the seed at the end, just to be safe
+
+
+@pytest.mark.parametrize("type,model", [(MoonClient, MoonModel(FeatureCnn(), HeadCnn()))])
+def test_popping_old_models_when_queue_fills(get_client: MoonClient) -> None:  # noqa
+    torch.manual_seed(42)
+    moon_client = get_client
+    moon_client.len_old_models_buffer = 2
+    assert len(moon_client.old_models_list) == 0
+    assert isinstance(moon_client.model, MoonModel)
+
+    original_base_module_params = [copy.deepcopy(val.cpu().numpy()) for val in moon_client.model.state_dict().values()]
+    assert len(original_base_module_params) > 0
+
+    moon_client.update_after_train(0, {})
+    moon_client.update_after_train(1, {})
+    assert len(moon_client.old_models_list) == 2
+
+    # Now we do a little "training," updating the model weights
+    for weights in moon_client.model.state_dict().values():
+        weights += 0.1
+
+    new_base_module_params = [copy.deepcopy(val.cpu().numpy()) for val in moon_client.model.state_dict().values()]
+    assert len(new_base_module_params) > 0
+
+    moon_client.update_after_train(2, {})
+    assert len(moon_client.old_models_list) == 2
+
+    # The first "old model" should have the original model parameters.
+    for old_model_params, original_params in zip(
+        moon_client.old_models_list[0].state_dict().values(), original_base_module_params
+    ):
+        assert (old_model_params.cpu().numpy() == original_params).all()
+
+    # The second "old model" should have the new model parameters
+    for old_model_params, new_params in zip(
+        moon_client.old_models_list[1].state_dict().values(), new_base_module_params
+    ):
+        assert (old_model_params.cpu().numpy() == new_params).all()
 
     torch.seed()  # resetting the seed at the end, just to be safe
