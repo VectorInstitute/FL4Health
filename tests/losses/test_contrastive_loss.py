Removing loop from the contrastive loss calculation, some small commenting improvements, adding a small test and fixing a test.

tests/losses/test_contrastive_loss.py
@@ -22,16 +22,45 @@ def test_contrastive_loss() -> None:
 
     assert pytest.approx(0.837868, abs=0.0001) == loss_value
 
-    features = torch.tensor([[[1.0, 1.0, 1.0]]]).float()
+    features = torch.tensor([[1.0, 1.0, 1.0]]).float()
     positive_pairs = torch.tensor([[[1.0, 1.0, 1.0]]]).float()
     negative_pairs = torch.tensor([[[0.0, 0.0, 0.0]]]).float()
     loss_value = contrastive_loss(features, positive_pairs, negative_pairs)
 
     assert loss_value == pytest.approx(0.1269, rel=0.01)
 
-    features = torch.tensor([[[0.0, 0.0, 0.0]]]).float()
+    features = torch.tensor([[0.0, 0.0, 0.0]]).float()
     positive_pairs = torch.tensor([[[1.0, 1.0, 1.0]]]).float()
     negative_pairs = torch.tensor([[[0.0, 0.0, 0.0]]]).float()
     loss_value = contrastive_loss(features, positive_pairs, negative_pairs)
 
     assert loss_value == pytest.approx(0.6931, rel=0.01)
+
+
+def test_compute_negative_similarities() -> None:
+    contrastive_loss = ContrastiveLoss(DEVICE)
+    features = torch.tensor([[1.0, 1.0, 1.0], [0.0, 0.0, 0.0]])
+    negative_pairs = torch.Tensor(
+        [[[1.0, 1.0, 1.0], [0.0, 0.0, 0.0]], [[-1.0, -1.0, -1.0], [1.0, 0.0, 0.0]], [[1.0, 0.0, 0.0], [0.0, 0.0, 0.0]]]
+    )
+
+    similarities = contrastive_loss.compute_negative_similarities(features, negative_pairs)
+
+    assert similarities.shape == torch.Size([3, 2])
+    assert torch.allclose(similarities[0], torch.Tensor([1.0, 0.0]), atol=1e-5)
+    assert torch.allclose(similarities[1], torch.Tensor([-1.0, 0.0]), atol=1e-5)
+    target_sim = 1.0 / torch.norm(torch.tensor([1.0, 1.0, 1.0])).item()
+    assert torch.allclose(similarities[2], torch.Tensor([target_sim, 0.0]), atol=1e-5)
+
+    # Test that when shapes mismatch, the computation fails.
+    features = torch.rand((4, 3))
+    negative_pairs_1 = torch.rand((5, 3, 3))
+    negative_pairs_2 = torch.rand((5, 4, 2))
+
+    # mismatch in batch dimension
+    with pytest.raises(AssertionError):
+        contrastive_loss.compute_negative_similarities(features, negative_pairs_1)
+
+    # mismatch in feature dimension
+    with pytest.raises(AssertionError):
+        contrastive_loss.compute_negative_similarities(features, negative_pairs_2)