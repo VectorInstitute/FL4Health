8b2bbc01617437c1569af6d786dd45357b0c9b72
Justin Yang <Yilin.yang@vectorinstitute.ai>
2024-06-06 17:54:58 -0700

    try adding a sampler

--- examples/flash_example/client.py
+++ examples/flash_example/client.py
@@ -20,11 +20,12 @@ from fl4health.utils.sampler import DirichletLabelBasedSampler
 
 class CifarFlashClient(FlashClient):
     def get_data_loaders(self, config: Config) -> Tuple[DataLoader, DataLoader]:
-        # sampler = DirichletLabelBasedSampler(list(range(10)), sample_percentage=0.75, beta=1)
+        sampler = DirichletLabelBasedSampler(list(range(10)), sample_percentage=0.75, beta=1)
         batch_size = self.narrow_config_type(config, "batch_size", int)
         train_loader, val_loader, _ = load_cifar10_data(
             self.data_path,
             batch_size,
+            sampler,
         )
         return train_loader, val_loader
 
