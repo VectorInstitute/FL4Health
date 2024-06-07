667812e1eaae1192c8b9d346e228e2896d7fa425
Justin Yang <Yilin.yang@vectorinstitute.ai>
2024-06-06 17:38:48 -0700

    local experiments arent running

--- examples/flash_example/client.py
+++ examples/flash_example/client.py
@@ -11,26 +11,25 @@ from torch.nn.modules.loss import _Loss
 from torch.optim import Optimizer
 from torch.utils.data import DataLoader
 
-from examples.models.cnn_model import MnistNet
+from examples.models.cnn_model import Net
 from fl4health.clients.flash_client import FlashClient
-from fl4health.utils.load_data import load_mnist_data
+from fl4health.utils.load_data import load_cifar10_data
 from fl4health.utils.metrics import Accuracy
 from fl4health.utils.sampler import DirichletLabelBasedSampler
 
 
-class MnistFlashClient(FlashClient):
+class CifarFlashClient(FlashClient):
     def get_data_loaders(self, config: Config) -> Tuple[DataLoader, DataLoader]:
-        sampler = DirichletLabelBasedSampler(list(range(10)), sample_percentage=0.75, beta=1)
+        # sampler = DirichletLabelBasedSampler(list(range(10)), sample_percentage=0.75, beta=1)
         batch_size = self.narrow_config_type(config, "batch_size", int)
-        train_loader, val_loader, _ = load_mnist_data(self.data_path, batch_size, sampler)
+        train_loader, val_loader, _ = load_cifar10_data(self.data_path, batch_size,)
         return train_loader, val_loader
 
     def get_model(self, config: Config) -> nn.Module:
-        return MnistNet().to(self.device)
+        return Net().to(self.device)
 
     def get_optimizer(self, config: Config) -> Optimizer:
-        # Note that the global optimizer operates on self.global_model.parameters()
-        return torch.optim.AdamW(self.model.parameters(), lr=0.01, weight_decay=0.001)
+        return torch.optim.SGD(self.model.parameters(), lr=0.001, momentum=0.9)
 
     def get_criterion(self, config: Config) -> _Loss:
         return torch.nn.CrossEntropyLoss()
@@ -53,7 +52,7 @@ if __name__ == "__main__":
     log(INFO, f"Device to be used: {DEVICE}")
     log(INFO, f"Server Address: {args.server_address}")
 
-    client = MnistFlashClient(data_path, [Accuracy()], DEVICE)
+    client = CifarFlashClient(data_path, [Accuracy("accuracy")], DEVICE)
     fl.client.start_client(server_address=args.server_address, client=client.to_client())
 
     # Shutdown the client gracefully
