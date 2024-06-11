225d933a3ca6b3a26af3c0d221aff2a735dd87e4
Justin Yang <Yilin.yang@vectorinstitute.ai>
2024-06-10 22:08:58 -0700

    try this change for codecheck

--- fl4health/strategies/flash.py
+++ fl4health/strategies/flash.py
@@ -125,8 +125,8 @@ class Flash(BasicFedAvg):
 
     def _update_d_t(self, delta_t: NDArrays, beta_3: NDArrays) -> None:
         """Update the drift-aware term d_t."""
-        # if not self.d_t:
-        #     self.d_t = [np.zeros_like(x) for x in delta_t]
+        if not self.d_t:
+            self.d_t = [np.zeros_like(x) for x in delta_t]
         for i, (delta, v_prev, d_prev) in enumerate(zip(delta_t, self.v_t, self.d_t)):
             d_t_j = []
             for j in range(len(delta)):
@@ -147,14 +147,14 @@ class Flash(BasicFedAvg):
 
     def _update_v_t(self, delta_t: NDArrays) -> None:
         """Update the second moment estimate v_t."""
-        # if self.v_t is None:
-        #     self.v_t = [np.zeros_like(x) for x in delta_t]
+        if self.v_t is None:
+            self.v_t = [np.zeros_like(x) for x in delta_t]
         self.v_t = [self.beta_2 * x + (1 - self.beta_2) * np.multiply(y, y) for x, y in zip(self.v_t, delta_t)]
 
     def _update_m_t(self, delta_t: NDArrays) -> None:
         """Update the first moment estimate m_t."""
-        # if self.m_t is None:
-        #     self.m_t = [np.zeros_like(x) for x in delta_t]
+        if self.m_t is None:
+            self.m_t = [np.zeros_like(x) for x in delta_t]
         self.m_t = [np.multiply(self.beta_1, x) + (1 - self.beta_1) * y for x, y in zip(self.m_t, delta_t)]
 
     def aggregate_fit(
@@ -180,9 +180,9 @@ class Flash(BasicFedAvg):
         #     self.v_t = [np.zeros_like(x) for x in delta_t]
         # if self.d_t is None:
         #     self.d_t = [np.zeros_like(x) for x in delta_t]
-        for attr in ["m_t", "v_t", "d_t"]:
-            if getattr(self, attr) is None:
-                setattr(self, attr, [np.zeros_like(x) for x in delta_t])
+        # for attr in ['m_t', 'v_t', 'd_t']:
+        #     if getattr(self, attr) is None:
+        #         setattr(self, attr, [np.zeros_like(x) for x in delta_t])
         # m_t
         self._update_m_t(delta_t)
 
@@ -195,6 +195,9 @@ class Flash(BasicFedAvg):
         self._update_d_t(delta_t, beta_3)
 
         # Update global weights
+        for attr in ['m_t', 'v_t', 'd_t']:
+            if getattr(self, attr) is None:
+                setattr(self, attr, [np.zeros_like(x) for x in delta_t])
         new_weights = [
             current_weight + self.eta * m_t / (np.sqrt(v_t) - d_t + self.tau)
             for current_weight, m_t, v_t, d_t in zip(self.current_weights, self.m_t, self.v_t, self.d_t)
