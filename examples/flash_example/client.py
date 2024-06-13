c66d7ff4e85da50a429d21ec81f4dd1035ffe329
Justin Yang <Yilin.yang@vectorinstitute.ai>
2024-06-13 11:20:43 -0700

    cleanup example client

--- examples/flash_example/client.py
+++ examples/flash_example/client.py
@@ -5,7 +5,6 @@ from pathlib import Path
 import flwr as fl
 import torch
 import torch.nn as nn
-from flwr.common.logger import log
 from flwr.common.typing import Config, Tuple
 from torch.nn.modules.loss import _Loss
 from torch.optim import Optimizer
@@ -15,17 +14,14 @@ from examples.models.cnn_model import Net
 from fl4health.clients.flash_client import FlashClient
 from fl4health.utils.load_data import load_cifar10_data
 from fl4health.utils.metrics import Accuracy
-from fl4health.utils.sampler import DirichletLabelBasedSampler
 
 
 class CifarFlashClient(FlashClient):
     def get_data_loaders(self, config: Config) -> Tuple[DataLoader, DataLoader]:
-        sampler = DirichletLabelBasedSampler(list(range(10)), sample_percentage=0.75, beta=1)
         batch_size = self.narrow_config_type(config, "batch_size", int)
         train_loader, val_loader, _ = load_cifar10_data(
             self.data_path,
             batch_size,
-            sampler,
         )
         return train_loader, val_loader
 
@@ -42,24 +38,11 @@ class CifarFlashClient(FlashClient):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="FL Client Main")
     parser.add_argument("--dataset_path", action="store", type=str, help="Path to the local dataset")
-    parser.add_argument(
-        "--server_address",
-        action="store",
-        type=str,
-        help="Server Address for the clients to communicate with the server through",
-        default="0.0.0.0:8080",
-    )
     args = parser.parse_args()
 
     DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     data_path = Path(args.dataset_path)
-    log(INFO, f"Device to be used: {DEVICE}")
-    log(INFO, f"Server Address: {args.server_address}")
-
-    client = CifarFlashClient(data_path, [Accuracy("accuracy")], DEVICE)
-    fl.client.start_client(server_address=args.server_address, client=client.to_client())
-
-    # Shutdown the client gracefully
+    client = FlashClient(data_path, [Accuracy("accuracy")], DEVICE)
+    fl.client.start_client(server_address="0.0.0.0:8080", client=client.to_client())
     client.shutdown()
 
-    client.metrics_reporter.dump()
