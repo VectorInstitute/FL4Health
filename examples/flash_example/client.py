99b9a51d7e4d26aea4d2a5e35bb35cbb5d791f53
Justin Yang <Yilin.yang@vectorinstitute.ai>
2024-06-13 12:49:21 -0700

    minor bug fix

--- examples/flash_example/client.py
+++ examples/flash_example/client.py
@@ -41,6 +41,6 @@ if __name__ == "__main__":
 
     DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     data_path = Path(args.dataset_path)
-    client = FlashClient(data_path, [Accuracy("accuracy")], DEVICE)
+    client = CifarFlashClient(data_path, [Accuracy("accuracy")], DEVICE)
     fl.client.start_client(server_address="0.0.0.0:8080", client=client.to_client())
     client.shutdown()
