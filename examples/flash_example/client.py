64bc954cfe36ed6e364be223a7090cd90fcbe6f8
Justin Yang <Yilin.yang@vectorinstitute.ai>
2024-06-13 13:49:47 -0700

    Try MNIST

--- examples/flash_example/client.py
+++ examples/flash_example/client.py
@@ -9,10 +9,11 @@ from torch.nn.modules.loss import _Loss
 from torch.optim import Optimizer
 from torch.utils.data import DataLoader
 
-from examples.models.cnn_model import Net
+from examples.models.cnn_model import Net, MnistNet
 from fl4health.clients.flash_client import FlashClient
-from fl4health.utils.load_data import load_cifar10_data
+from fl4health.utils.load_data import load_cifar10_data, load_mnist_data
 from fl4health.utils.metrics import Accuracy
+from fl4health.utils.sampler import DirichletLabelBasedSampler
 
 
 class CifarFlashClient(FlashClient):
@@ -32,6 +33,22 @@ class CifarFlashClient(FlashClient):
 
     def get_criterion(self, config: Config) -> _Loss:
         return torch.nn.CrossEntropyLoss()
+    
+class MNISTFlashClient(FlashClient):
+    def get_data_loaders(self, config: Config) -> Tuple[DataLoader, DataLoader]:
+        sampler = DirichletLabelBasedSampler(list(range(10)), sample_percentage=0.75, beta=1)
+        batch_size = self.narrow_config_type(config, "batch_size", int)
+        train_loader, val_loader, _ = load_mnist_data(self.data_path, batch_size, sampler)
+        return train_loader, val_loader
+
+    def get_model(self, config: Config) -> nn.Module:
+        return MnistNet().to(self.device)
+
+    def get_optimizer(self, config: Config) -> Optimizer:
+        return torch.optim.AdamW(self.model.parameters(), lr=0.01)
+
+    def get_criterion(self, config: Config) -> _Loss:
+        return torch.nn.CrossEntropyLoss()
 
 
 if __name__ == "__main__":
@@ -41,6 +58,6 @@ if __name__ == "__main__":
 
     DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     data_path = Path(args.dataset_path)
-    client = CifarFlashClient(data_path, [Accuracy("accuracy")], DEVICE)
+    client = MNISTFlashClient(data_path, [Accuracy("accuracy")], DEVICE)
     fl.client.start_client(server_address="0.0.0.0:8080", client=client.to_client())
     client.shutdown()
