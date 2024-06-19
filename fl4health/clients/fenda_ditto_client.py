deb71a39043136ed0d634f8cd89c577984baf9c3
Justin Yang <Yilin.yang@vectorinstitute.ai>
2024-06-19 10:21:03 -0700

    Initial commit of Fenda+Ditto

---
+++ fl4health/clients/fenda_ditto_client.py
@@ -0,0 +1,236 @@
+from logging import INFO
+from pathlib import Path
+from typing import Dict, List, Optional, Sequence, Tuple
+
+import torch
+import torch.nn as nn
+from flwr.common.logger import log
+from flwr.common.typing import Config, NDArrays, Scalar
+
+from fl4health.model_bases.fenda_base import FendaModel
+from fl4health.losses.weight_drift_loss import WeightDriftLoss
+from fl4health.utils.losses import TrainingLosses
+from fl4health.utils.metrics import Metric
+from fl4health.clients.basic_client import BasicClient, TorchInputType
+from fl4health.checkpointing.client_module import ClientCheckpointModule
+from fl4health.utils.losses import LossMeterType
+from fl4health.parameter_exchange.full_exchanger import FullParameterExchanger
+from fl4health.utils.losses import LossMeterType, TrainingLosses
+
+
+class FendaDittoGlobalModel(nn.Module):
+    def __init__(self, feature_extractor: nn.Module, classifier: nn.Module) -> None:
+        super().__init__()
+        self.feature_extractor = feature_extractor
+        self.classifier = classifier
+
+    def layers_to_exchange(self) -> List[str]:
+        return [
+            layer_name for layer_name in self.state_dict().keys() if layer_name.startswith("feature_extractor.")
+        ]
+
+class FendaDittoClient(BasicClient):
+    def __init__(
+        self,
+        data_path: Path,
+        metrics: Sequence[Metric],
+        device: torch.device,
+        loss_meter_type: LossMeterType = LossMeterType.AVERAGE,
+        checkpointer: Optional[ClientCheckpointModule] = None,
+        lam: float = 1.0,
+        freeze_global_feature_extractor: bool = False,
+    ) -> None:
+        super().__init__(
+            data_path=data_path,
+            metrics=metrics,
+            device=device,
+            loss_meter_type=loss_meter_type,
+            checkpointer=checkpointer,
+        )
+        self.initial_global_tensors: List[torch.Tensor]
+        self.lam = lam
+        self.global_model: FendaDittoGlobalModel
+        self.model: FendaModel
+        self.ditto_drift_loss_function = WeightDriftLoss(self.device)
+        self.freeze_global_feature_extractor = freeze_global_feature_extractor
+
+    def get_model(self, config: Config) -> FendaModel:
+        """
+        User defined method that returns FENDA-FL model.
+
+        Args:
+            config (Config): The config from the server.
+
+        Returns:
+            FendaModel: The client FENDA-FL model.
+
+        Raises:
+            NotImplementedError: To be defined in child class.
+        """
+        raise NotImplementedError
+    
+    def get_global_model(self, config: Config) -> FendaDittoGlobalModel:
+        """
+        User defined method that returns a Global Feature Model that is compatible with FENDA-FL model.
+
+        Args:
+            config (Config): The config from the server.
+
+        Returns:
+            FendaModel: The client FENDA-FL model.
+
+        Raises:
+            NotImplementedError: To be defined in child class.
+        """
+        raise NotImplementedError
+    
+    def setup_client(self, config: Config) -> None:
+        assert self.checkpointer.pre_aggregation is not None, "self.checkpointer.pre_aggregation must be present"
+        self.global_model = self.get_model(config).to(self.device)
+        super().setup_client(config)
+
+    def get_parameters(self, config: Config) -> NDArrays:
+        assert self.global_model is not None and self.parameter_exchanger is not None
+        return self.parameter_exchanger.push_parameters(self.global_model, config=config)
+
+    def set_parameters(self, parameters: NDArrays, config: Config, fitting_round: bool) -> None:
+        assert self.global_model is not None and self.model is not None
+        assert self.parameter_exchanger is not None and isinstance(self.parameter_exchanger, FullParameterExchanger)
+
+        current_server_round = self.narrow_config_type(config, "current_server_round", int)
+        if current_server_round == 1 and fitting_round:
+            log(INFO, "Initializing the global and local models weights for the first time")
+            self.initialize_all_model_weights(parameters, config)
+        else:
+            log(INFO, "Setting the global model weights")
+            self.parameter_exchanger.pull_parameters(parameters, self.global_model, config)
+            self.model.second_feature_extractor = self.global_model.feature_extractor # feature extracor is given to FENDA model
+
+    def update_before_train(self, current_server_round: int) -> None:
+        self.initial_global_tensors = [
+            initial_layer_weights.detach().clone()
+            for layer_name, initial_layer_weights in self.global_model.state_dict().items()
+            if layer_name.startswith("feature_extractor.")
+        ]
+        return super().update_before_train(current_server_round)
+
+    def initialize_all_model_weights(self, parameters: NDArrays, config: Config) -> None:
+        self.parameter_exchanger.pull_parameters(parameters, self.global_model, config)
+        self.model.second_feature_extractor = self.global_model # feature extracor is given to FENDA model
+
+    def train_by_epochs(
+        self, epochs: int, current_round: Optional[int] = None
+    ) -> Tuple[Dict[str, float], Dict[str, Scalar]]:
+        """
+        Train locally for the specified number of epochs.
+
+        Args:
+            epochs (int): The number of epochs for local training.
+            current_round (Optional[int]): The current FL round.
+
+        Returns:
+            Tuple[Dict[str, float], Dict[str, Scalar]]: The loss and metrics dictionary from the local training.
+                Loss is a dictionary of one or more losses that represent the different components of the loss.
+        """
+        if self.freeze_global_feature_extractor:
+            for param in self.model.second_feature_extractor.parameters():
+                param.requires_grad = False
+        return super().train_by_epochs(epochs, current_round)
+
+    def train_by_steps(
+        self, steps: int, current_round: Optional[int] = None
+    ) -> Tuple[Dict[str, float], Dict[str, Scalar]]:
+        """
+        Train locally for the specified number of steps.
+
+        Args:
+            steps (int): The number of steps to train locally.
+
+        Returns:
+            Tuple[Dict[str, float], Dict[str, Scalar]]: The loss and metrics dictionary from the local training.
+                Loss is a dictionary of one or more losses that represent the different components of the loss.
+        """
+        if self.freeze_global_feature_extractor:
+            for param in self.model.second_feature_extractor.parameters():
+                param.requires_grad = False
+        return super().train_by_steps(steps, current_round)
+
+    def train_step(
+        self, input: TorchInputType, target: torch.Tensor
+    ) -> Tuple[TrainingLosses, Dict[str, torch.Tensor]]:
+        """
+        Mechanics of training loop follow from original Ditto implementation: https://github.com/litian96/ditto
+        As in the implementation there, steps of the global and local models are done in tandem and for the same
+        number of steps.
+
+        Args:
+            input (TorchInputType): input tensor to be run through
+            both the global and local models. Here, TorchInputType is simply an alias
+            for the union of torch.Tensor and Dict[str, torch.Tensor].
+            target (torch.Tensor): target tensor to be used to compute a loss given each models outputs.
+
+        Returns:
+            Tuple[TrainingLosses, Dict[str, torch.Tensor]]: Returns relevant loss values from both the global and local
+                model optimization steps. The prediction dictionary contains predictions indexed a "global" and "local"
+                corresponding to predictions from the global and local Ditto models for metric evaluations.
+        """
+
+        # Clear gradients from optimizers if they exist
+        self.optimizers["global"].zero_grad()
+        self.optimizers["local"].zero_grad()
+
+        # Forward pass on both the global and local models
+        preds, features = self.predict(input)
+
+        # Compute all relevant losses
+        # NOTE: features here should be a blank dictionary, as we're not using them
+        assert len(features) == 0
+        losses = self.compute_training_loss(preds, features, target)
+
+        # Take a step with the global model vanilla loss
+        losses.additional_losses["global_loss"].backward()
+        self.optimizers["global"].step()
+
+        # Take a step with the local model using the local loss and Ditto constraint
+        losses.backward["backward"].backward()
+        self.optimizers["local"].step()
+
+        # Return dictionary of predictions where key is used to name respective MetricMeters
+        return losses, preds
+
+    def compute_training_loss(
+        self,
+        preds: Dict[str, torch.Tensor],
+        features: Dict[str, torch.Tensor],
+        target: torch.Tensor,
+    ) -> TrainingLosses:
+        """
+        Computes training losses given predictions of the global and local models and ground truth data.
+        For the local model we add to the vanilla loss function by including Ditto penalty loss which is the l2 inner
+        product between the initial global model weights and weights of the local model. This is stored in backward
+        The loss to optimize the global model is stored in the additional losses dictionary under "global_loss"
+
+        Args:
+            preds (Dict[str, torch.Tensor]): Prediction(s) of the model(s) indexed by name.
+                All predictions included in dictionary will be used to compute metrics.
+            features: (Dict[str, torch.Tensor]): Feature(s) of the model(s) indexed by name.
+            target: (torch.Tensor): Ground truth data to evaluate predictions against.
+
+        Returns:
+            TrainingLosses: an instance of TrainingLosses containing backward loss and
+                additional losses indexed by name. Additional losses includes each loss component and the global model
+                loss tensor.
+        """
+        # Check that both models are in training mode
+        assert self.model.training
+
+        loss, additional_losses = self.compute_loss_and_additional_losses(preds, features, target)
+
+        # Compute ditto drift loss
+        if self.freeze_global_feature_extractor:
+            ditto_local_loss = self.ditto_drift_loss_function(self.model.first_feature_extractor, self.initial_global_tensors, self.lam)
+        else:
+            ditto_local_loss = self.ditto_drift_loss_function(self.model.second_feature_extractor, self.initial_global_tensors, self.lam)
+        additional_losses["ditto_loss"] = ditto_local_loss.clone()
+
+        return TrainingLosses(backward=loss + ditto_local_loss, additional_losses=additional_losses)
