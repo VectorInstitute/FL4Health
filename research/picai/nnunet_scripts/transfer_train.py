f81b7fde8712eba27745817f4fa6ae666f9cec9b
Shawn Carere <shawncarere@gmail.com>
2024-06-25 11:58:29 -0400

    Addressed Johns comments and changed one of the arguments which led to a bit of a code reorg

--- research/picai/nnunet_scripts/transfer_train.py
+++ research/picai/nnunet_scripts/transfer_train.py
@@ -1,14 +1,4 @@
-"""Automates simple transfer learning for nnUNetv2
-
-    Assumptions:
-        - nnUNetv2 must be properly set up (ie. required environment variables set)
-        - The finetuning dataset must be properly formatted and in the nnUNet_raw folder
-        - A plans file for the pretrained model must be present in the nnUNnet_preprocessed
-            under the folder of the dataset it was pretrained on.
-        - The dimensions and voxel spacings of the finetuning dataset must
-            be the same as the pretraining dataset
-        - The pretrained model must already be saved and stored as a pytorch ckpt
-"""
+"Automates simple transfer learning for nnUNetv2"
 
 import argparse
 import os
@@ -18,20 +8,22 @@ from multiprocessing import Process
 import torch
 from batchgenerators.utilities.file_and_folder_operations import load_json, save_json
 from nnunetv2.experiment_planning.plan_and_preprocess_api import extract_fingerprints, preprocess_dataset
-from nnunetv2.experiment_planning.plans_for_pretraining.move_plans_between_datasets import move_plans_between_datasets
 from nnunetv2.paths import nnUNet_preprocessed, nnUNet_raw
 from nnunetv2.run.run_training import run_training
 from nnunetv2.utilities.dataset_name_id_conversion import convert_id_to_dataset_name
 
 
 def setup_device(device: str) -> torch.device:
-    """Checks the device being used, sets the number of threads and returns a pytorch device
+    """
+    Checks the device being used, sets the number of threads and returns a
+    pytorch device
+
 
     Args:
-        device: Either 'cpu', 'cuda' or 'mps'
+        device (str): Either 'cpu', 'cuda' or 'mps'
 
-    Returns
-        torch_device: The corresponding pytorch.device object
+    Returns:
+        torch.device: The corresponding pytorch.device object
     """
     assert device in [
         "cpu",
@@ -53,58 +45,95 @@ def setup_device(device: str) -> torch.device:
         return torch.device("mps")
 
 
-def transfer_metadata(ft_id: int, pt_id: int, pt_plans_identifier: str) -> str:
-    """Transfers and or modifies metadata necessary for transfer learning
-
-    Ensures the dataset json, dataset fingerprint and nnunet plans are all
-    ready for transfer learning. Some necessary changes are made to the plans
-    from pretraining to allow finetuning to occur without issue. Note that
-    this function saves the plans for finetuning as 'pt_plans_identifier-ftXptY'
-    where X is ft_if and Y is pt_id. If a plan file is already found that
-    matches this pattern, the function does not overwrite it.
+def move_plans_between_datasets(finetune_id: int, pretrain_plans: str, finetune_plans_identifier: str) -> None:
+    """
+    Transfers the necessary files needed from pretraining to the relevant
+    finetuning directories. Makes changes to the plans file to ensure
+    finetuning runs without issue
 
     Args:
-        ft_id: The nnunet id of the finetuning dataset
-        pt_id: The nnunet id of the pretraining dataset
-        pt_plans_identifier: The identifier/name of the plans from pretraining
-            to use as a basis for the finetuning plans
+        finetune_id (int): The nnunet dataset id for the finetuning dataset
+        pretrain_plans (str): Path to the nnunet plans json used for
+            pretraining
+        finetune_plans_identifier (str): What to name the plans for
+            finetuning (no extension)
+    """
+    ft_dataset_name = convert_id_to_dataset_name(finetune_id)
+    plans = load_json(pretrain_plans)  # Will convert plans into ft_plans
+
+    # Change the dataset name
+    plans["dataset_name"] = ft_dataset_name
+
+    # Define some variables needed for making further changes to plans file
+    pt_plans_identifier = os.path.basename(pretrain_plans).split(".")[0]
+    num_tr = load_json(os.path.join(nnUNet_raw, ft_dataset_name, "dataset.json"))[
+        "numTraining"
+    ]  # Number of training samples
+    bs_5percent = round(num_tr * 0.05)
+
+    # Edit parameters for each configuration
+    for c in plans["configurations"].keys():
+        # Change the data identifier
+        if "data_identifier" in plans["configurations"][c].keys():
+            old_data_identifier = plans["configurations"][c]["data_identifier"]
+            if old_data_identifier.startswith(pt_plans_identifier):
+                # The data identifier is typically a combination of the plans identifier and the config
+                new_data_identifier = finetune_plans_identifier + old_data_identifier[len(pt_plans_identifier) :]
+            else:
+                new_data_identifier = finetune_plans_identifier + "_" + old_data_identifier
+            plans["configurations"][c]["data_identifier"] = new_data_identifier
+
+        # Ensure the batch size is within nnUNet limits
+        if "batch_size" in plans["configurations"][c].keys():
+            old_bs = plans["configurations"][c]["batch_size"]
+            new_bs = max(min(old_bs, bs_5percent), 2)
+            plans["configurations"][c]["batch_size"] = new_bs
+
+    ft_plans_path = os.path.join(nnUNet_preprocessed, ft_dataset_name, finetune_plans_identifier + ".json")
+    save_json(plans, ft_plans_path)
+
+
+def transfer_metadata(finetune_id: int, pretrain_id: int, pretrain_plans: str) -> str:
+    """
+    Transfers and or modifies metadata necessary for transfer learning
+
+    Ensures the dataset json, dataset fingerprint and nnunet plans are all
+    ready for transfer learning. Some necessary changes are made to the
+    plans from pretraining to allow finetuning to occur without issue.
+    Note that this function saves the plans for finetuning as
+    'pt_plans_identifier-ptABC' where ABC is pretrain_id. If a plan file
+    is already found that matches this pattern, the function does not
+    overwrite it.
+
+
+    Args:
+        finetune_id (int): The nnunet id of the finetuning dataset
+        pretrain_id (int): The nnunet id of the pretraining dataset
+        pretrain_plans (str): Path to the nnunet plans json used for
+            pretraining
+
     Returns:
-        tl_plans_identifier: The identifier for the plans to use during finetuning
+        str: The name of the plans file for finetuning (without an
+            extension)
     """
     # Extract Fingerprint if needed
-    ft_dataset_name = convert_id_to_dataset_name(ft_id)
+    ft_dataset_name = convert_id_to_dataset_name(finetune_id)
     fingerprint_path = os.path.join(nnUNet_preprocessed, ft_dataset_name, "dataset_fingerprint.json")
     if not os.path.isfile(fingerprint_path):
-        extract_fingerprints(dataset_ids=[ft_id])
+        extract_fingerprints(dataset_ids=[finetune_id])
     else:
         print("INFO: The finetuning dataset fingerprint has already been extracted. Skipping fingerprint extraction")
 
     # Transfer pretrained model plans if needed
-    tl_plans_identifier = pt_plans_identifier + "-pt%03.0d" % pt_id
-    tl_plans_path = os.path.join(nnUNet_preprocessed, ft_dataset_name, tl_plans_identifier + ".json")
-    if not os.path.isfile(tl_plans_path):
-        # This automatically changes the dataset name and data identifier fields
-        # of the plans file to match the finetuning/target dataset
+    pt_plans_identifier = os.path.basename(pretrain_plans).split(".")[0]
+    ft_plans_identifier = pt_plans_identifier + "-pt%03.0d" % pretrain_id
+    ft_plans_path = os.path.join(nnUNet_preprocessed, ft_dataset_name, ft_plans_identifier + ".json")
+    if not os.path.isfile(ft_plans_path):
         move_plans_between_datasets(
-            source_dataset_name_or_id=pt_id,
-            target_dataset_name_or_id=ft_id,
-            source_plans_identifier=pt_plans_identifier,
-            target_plans_identifier=tl_plans_identifier,
+            finetune_id=finetune_id, pretrain_plans=pretrain_plans, finetune_plans_identifier=ft_plans_identifier
         )
-
-        # The batch size needs to be adjusted for the new finetuning dataset as
-        # its upper bound is determined by the number of samples in the dataset
-        num_tr = load_json(os.path.join(nnUNet_raw, ft_dataset_name, "dataset.json"))["numTraining"]
-        bs_5percent = round(num_tr * 0.05)  # The upper bound for batch size
-        tl_plans = load_json(tl_plans_path)
-        for c in tl_plans["configurations"]:  # Must check batch size for each config
-            pt_bs = tl_plans["configurations"][c]["batch_size"]
-            # Ensure batch size is below upper bound and above lower bound of 2
-            bs = max(min(pt_bs, bs_5percent), 2)
-            tl_plans["configurations"][c]["batch_size"] = bs
-        save_json(tl_plans, tl_plans_path, sort_keys=False)
     else:
-        print("INFO: The {tl_plans_identifier} plans file already exists. Using that instead")
+        print("INFO: The {ft_plans_identifier} plans file already exists at {ft_plans_path}. Using that instead")
 
     # Copy dataset json if needed
     if not os.path.isfile(os.path.join(nnUNet_preprocessed, ft_dataset_name, "dataset.json")):
@@ -113,57 +142,51 @@ def transfer_metadata(ft_id: int, pt_id: int, pt_plans_identifier: str) -> str:
             os.path.join(nnUNet_preprocessed, ft_dataset_name, "dataset.json"),
         )
 
-    return tl_plans_identifier
+    return ft_plans_identifier
 
 
-def check_configs(id: int, plans_identifier: str, configs: list) -> None:
-    """Raises an error if configs are not found in plans json
+def check_configs(plans_path: str, configs: list) -> None:
+    """
+    Raises an error if configs are not found in plans json
 
     Args:
-        id: The nnunet id of the dataset for which the plans were created
-        plans_identifier: The name of the plans file to check, not including
-            any extenstions
-        configs: A list of nnunet configs to check for
+        plans (str): Path to the plans file to check
+        configs (list): A list of nnunet configs to check for
     """
-    dataset_name = convert_id_to_dataset_name(id)
-    plans_path = os.path.join(nnUNet_preprocessed, dataset_name, plans_identifier + ".json")
     plans = load_json(plans_path)
     for c in configs:
         assert (
             c in plans["configurations"]
-        ), f"Did not find {c} config in {plans_identifier}.json. Note that \
+        ), f"Did not find {c} config in {plans_path}. Note that \
             3d_lowres and 3d_cascade_fullres are not created for datasets with \
             small image sizes"
 
 
 def main() -> None:
-    """Parses arguments and opens subprocesses to train specified models
-
-    Notes: Subqequent trainings after the first model training must run in
-        subprocesses otherwise pytorch will refuse to use gpu for some reason.
+    """
+    Parses arguments and opens subprocesses to train specified models
     """
     parser = argparse.ArgumentParser()
 
     parser.add_argument(
-        "--ft_id", type=int, required=True, help="The nnUNet dataset ID integer for the fine-tuning dataset"
-    )
-    parser.add_argument(
-        "--pt_id",
+        "--finetune_id",
         type=int,
         required=True,
-        help="[OPTIONAL] The dataset ID integer of the pretraining dataset"
-        ". Can be omitted if pt_fingerprint, pt_plans and pt_identifier"
-        "flags are all included",
+        help="The nnUNet dataset ID \
+            integer for the fine-tuning dataset",
     )
-    parser.add_argument("--pt_plans_identifier", type=str, required=False, default="nnUNetPlans")
     parser.add_argument(
-        "--pt_checkpoints",
-        type=str,
+        "--pretrain_id",
+        type=int,
         required=True,
-        nargs="+",
-        help="Path to the model checkpoints of the pretrained models."
-        " One checkpoint must be provided for each config and the order must be"
-        " consistent with the --configs flag",
+        help="The nnunet dataset ID integer of the pretraining dataset. \
+            if the pretrain plans are given using the --pretrain_plans flag, \
+            then pretrain id is only used for filenaming purposes and can be set \
+            to any arbitrary id so long as the id is not in use by another \
+            nnunet dataset",
+    )
+    parser.add_argument(
+        "--fold", type=int, required=True, help="Fold of the 5-fold cross-validation. Should be an int between 0 and 4"
     )
     parser.add_argument(
         "--configs",
@@ -177,16 +200,29 @@ def main() -> None:
         " 3d_lowres already be trained first",
     )
     parser.add_argument(
-        "--fold", type=int, required=True, help="Fold of the 5-fold cross-validation. Should be an int between 0 and 4"
+        "--pretrain_checkpoints",
+        type=str,
+        required=True,
+        nargs="+",
+        help="Path to the model checkpoints of the pretrained models."
+        " One checkpoint must be provided for each config and the order must be"
+        " consistent with the --configs flag",
     )
     parser.add_argument(
-        "-np",
+        "--pretrain_plans",
+        type=str,
+        required=False,
+        help="[OPTIONAL] The path to the nnUNet plans used for pretraining. \
+            Defaults to looking for nnUNetPlans.json in the pretraining \
+            datasets nnUNet_preprocessed folder",
+    )
+    parser.add_argument(
+        "-num_processes",
         type=int,
         nargs="+",
-        default=[8, 4, 8],
         required=False,
         help="[OPTIONAL] Use this to define how many processes are to be used. If this is just one number then "
-        "this number of processes is used for all configurations specified with -c. If it's a "
+        "this number of processes is used for all configurations specified with --configs. If it's a "
         "list of numbers this list must have as many elements as there are configurations. We "
         "then iterate over zip(configs, num_processes) to determine then umber of processes "
         "used for each configuration. More processes is always faster (up to the number of "
@@ -215,7 +251,7 @@ def main() -> None:
         "and finetuning dataset",
     )
     parser.add_argument(
-        "-tr",
+        "--trainer",
         type=str,
         default="nnUNetTrainer",
         required=False,
@@ -225,28 +261,41 @@ def main() -> None:
 
     args = parser.parse_args()
 
-    check_configs(id=args.pt_id, plans_identifier=args.pt_plans_identifier, configs=args.configs)
+    # If pretrain plans path not given, set default pretrain plans path
+    if args.pretrain_plans is None:
+        pt_dataset_name = convert_id_to_dataset_name(args.pretrain_id)
+        args.pretrain_plans = os.path.join(nnUNet_preprocessed, pt_dataset_name, "nnUNetPlans.json")
 
+    # Ensure all configs provided are in the original pretraining plans
+    check_configs(plans_path=args.pretrain_plans, configs=args.configs)
+
+    # Ensure a checkpoint was provided for each config
     assert len(args.configs) == len(
-        args.pt_checkpoints
-    ), f"The number of configs ({len(args.configs)}) and number of pretrained \
-        checkpoints ({len(args.pt_checkpoints)}) do not match"
+        args.pretrain_checkpoints
+    ), f"The number of \
+        configs ({len(args.configs)}) and number of pretrained checkpoints \
+        ({len(args.pretrain_checkpoints)}) do not match"
 
     # Transfer Metadata needed for processing the finetuning set
-    tl_plans_identifier = transfer_metadata(
-        ft_id=args.ft_id, pt_id=args.pt_id, pt_plans_identifier=args.pt_plans_identifier
+    ft_plans_identifier = transfer_metadata(
+        finetune_id=args.finetune_id, pretrain_id=args.pretrain_id, pretrain_plans=args.pretrain_plans
     )
 
     # Set default number of processes for preprocessing
-    if args.np is None:
-        default_np = {"2d": 8, "3d_fullres": 4, "3d_lowres": 8}
-        np = [default_np[c] if c in default_np.keys() else 4 for c in args.configs]
+    if args.num_processes is None:
+        default_num_processes = {"2d": 8, "3d_fullres": 4, "3d_lowres": 8}
+        num_processes = [default_num_processes[c] if c in default_num_processes.keys() else 4 for c in args.configs]
     else:
-        np = args.np
+        num_processes = args.num_processes
 
     # preprocessing
     if not args.skip_preprocessing:
-        preprocess_dataset(dataset_id=args.ft_id, plans_identifier=tl_plans_identifier, num_processes=np)
+        preprocess_dataset(
+            dataset_id=args.finetune_id,
+            plans_identifier=ft_plans_identifier,
+            num_processes=num_processes,
+            configurations=args.configs,
+        )
 
     device = setup_device(args.device)
     # We enforce the models to train in the below order. Namely because 3d_lowres
@@ -256,13 +305,13 @@ def main() -> None:
     for config in possible_configs:
         if config in args.configs:
             i = args.configs.index(config)
-            pt_ckpt = args.pt_checkpoints[i]
+            pt_ckpt = args.pretrain_checkpoints[i]
             kwargs = {
-                "dataset_name_or_id": str(args.ft_id),
+                "dataset_name_or_id": str(args.finetune_id),
                 "configuration": config,
                 "fold": args.fold,
-                "trainer_class_name": args.tr,
-                "plans_identifier": tl_plans_identifier,
+                "trainer_class_name": args.trainer,
+                "plans_identifier": ft_plans_identifier,
                 "pretrained_weights": pt_ckpt,
                 "device": device,
             }
