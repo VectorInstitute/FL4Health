4c87a483db4fc9c0519dc49c95d550c09e9f04f7
Shawn Carere <shawncarere@gmail.com>
2024-07-04 18:17:53 -0400

    WIP: forgot some files

--- research/picai/metrics_utils.py
+++ research/picai/metrics_utils.py
@@ -8,33 +8,6 @@ from picai_eval import evaluate
 from fl4health.utils.metrics import Metric
 
 
-class nnUNetMetric(Metric):
-    def __init__(self, metric: Metric):
-        super().__init__(name=metric.name)
-        self.metric = metric
-
-    def update(self, pred: torch.Tensor, target: torch.Tensor) -> None:
-        # nnUNet models output seperate channels for each class (even in binary segmentation)
-        # However labels are not onehot encoded to match leading to a dimension error when using torchmetrics
-        # We only have a single target
-        if pred.ndim != target.ndim:  # Add channel dimension if there isn't one
-            target = target.view(target.shape[0], 1, *target.shape[1:])
-
-        if pred.shape != target.shape:
-            target_one_hot = torch.zeros(pred.shape, device=pred.device, dtype=torch.bool)
-            target_one_hot.scatter(
-                1, target.long(), 1
-            )  # This does the onehot encoding. Its a weird function that is not intuitive
-
-        self.metric.update(pred.long(), target_one_hot)
-
-    def compute(self, name: Optional[str]) -> Metrics:
-        return self.metric.compute(name=name)
-
-    def clear(self) -> None:
-        self.metric.clear()
-
-
 class PICAI_AUROC(torchmetrics.Metric):
     def __init__(self, **kwargs: Any) -> None:
         """
