053e4fc3f0a6ee09fc27e3c781d9b6944e97d157
sanaAyrml <ayromlous@gmail.com>
2024-09-24 00:31:42 -0400

    Revert "Delete deep mmd files"
    This reverts commit 1f213c314990693b61e6fd4d26c732f6073e4f77.

---
+++ fl4health/clients/deep_mmd_clients/ditto_deep_mmd_client.py
@@ -0,0 +1,197 @@
+from logging import ERROR
+from pathlib import Path
+from typing import Dict, Optional, Sequence, Tuple
+
+import torch
+import torch.nn as nn
+from flwr.common.logger import log
+from flwr.common.typing import Config, Scalar
+
+from fl4health.checkpointing.client_module import CheckpointMode, ClientCheckpointModule
+from fl4health.clients.ditto_client import DittoClient
+from fl4health.losses.deep_mmd_loss import DeepMmdLoss
+from fl4health.model_bases.feature_extractor_buffer import FeatureExtractorBuffer
+from fl4health.utils.losses import EvaluationLosses, LossMeterType
+from fl4health.utils.metrics import Metric
+from fl4health.utils.typing import TorchFeatureType, TorchInputType, TorchPredType, TorchTargetType
+
+
+class DittoDeepMmdClient(DittoClient):
+    def __init__(
+        self,
+        data_path: Path,
+        metrics: Sequence[Metric],
+        device: torch.device,
+        loss_meter_type: LossMeterType = LossMeterType.AVERAGE,
+        checkpointer: Optional[ClientCheckpointModule] = None,
+        lam: float = 1.0,
+        deep_mmd_loss_weight: float = 10.0,
+        flatten_feature_extraction_layers: Dict[str, bool] = {},
+        size_feature_extraction_layers: Dict[str, int] = {},
+    ) -> None:
+        """
+        This client implements the DEEP-MMD loss function in the Ditto framework.
+        Args:
+            data_path (Path): path to the data to be used to load the data for client-side training
+            metrics (Sequence[Metric]): Metrics to be computed based on the labels and predictions of the client model
+            device (torch.device): Device indicator for where to send the model, batches, labels etc. Often 'cpu' or
+                'cuda'
+            loss_meter_type (LossMeterType, optional): Type of meter used to track and compute the losses over
+                each batch. Defaults to LossMeterType.AVERAGE.
+            checkpointer (Optional[TorchCheckpointer], optional): Checkpointer to be used for client-side
+                checkpointing. Defaults to None.
+            metrics_reporter (Optional[MetricsReporter], optional): A metrics reporter instance to record the metrics
+                during the execution. Defaults to an instance of MetricsReporter with default init parameters.
+            lam (float, optional): weight applied to the Ditto drift loss. Defaults to 1.0.
+            deep_mmd_loss_weight (float, optional): weight applied to the DEEP-MMD loss. Defaults to 10.0.
+            flatten_feature_extraction_layers (Dict[str, bool], optional): Dictionary of layers to extract features
+                from them what is the flattened feature size. Defaults to {}. If it is -1 then the layer is not
+                flattened.
+        """
+        super().__init__(
+            data_path=data_path,
+            metrics=metrics,
+            device=device,
+            loss_meter_type=loss_meter_type,
+            checkpointer=checkpointer,
+            lam=lam,
+        )
+        self.deep_mmd_loss_weight = deep_mmd_loss_weight
+        if self.deep_mmd_loss_weight == 0:
+            log(
+                ERROR,
+                "DEEP MMD loss weight is set to 0. As DEEP-MMD loss will not be computed, ",
+                "please use vanilla DittoClient instead.",
+            )
+
+        self.flatten_feature_extraction_layers = flatten_feature_extraction_layers
+        self.size_feature_extraction_layers = size_feature_extraction_layers
+        self.deep_mmd_losses = {}
+        for layer in self.flatten_feature_extraction_layers.keys():
+            self.deep_mmd_losses[layer] = DeepMmdLoss(
+                device=self.device,
+                input_size=self.size_feature_extraction_layers[layer],
+                layer_name=layer,
+            ).to(self.device)
+
+        self.init_global_model: nn.Module
+        self.local_feature_extractor: FeatureExtractorBuffer
+        self.init_global_feature_extractor: FeatureExtractorBuffer
+
+    def setup_client(self, config: Config) -> None:
+        super().setup_client(config)
+        self.local_feature_extractor = FeatureExtractorBuffer(
+            model=self.model,
+            flatten_feature_extraction_layers=self.flatten_feature_extraction_layers,
+        )
+
+    def update_before_train(self, current_server_round: int) -> None:
+        super().update_before_train(current_server_round)
+        assert isinstance(self.global_model, nn.Module)
+        # Register hooks to extract features from the local model if not already registered
+        self.local_feature_extractor._maybe_register_hooks()
+        # Clone and freeze the initial weights GLOBAL MODEL. These are used to form the Ditto local
+        # update penalty term.
+        self.init_global_model = self.clone_and_freeze_model(self.global_model)
+        self.init_global_feature_extractor = FeatureExtractorBuffer(
+            model=self.init_global_model,
+            flatten_feature_extraction_layers=self.flatten_feature_extraction_layers,
+        )
+        # Register hooks to extract features from the init global model if not already registered
+        self.init_global_feature_extractor._maybe_register_hooks()
+
+    def predict(
+        self,
+        input: TorchInputType,
+    ) -> Tuple[Dict[str, torch.Tensor], Dict[str, torch.Tensor]]:
+        """
+        Computes the predictions for both the GLOBAL and LOCAL models and pack them into the prediction dictionary
+
+        Args:
+            input (Union[torch.Tensor, Dict[str, torch.Tensor]]): Inputs to be fed into both models.
+
+        Returns:
+            Tuple[Dict[str, torch.Tensor], Dict[str, torch.Tensor]]: A tuple in which the first element
+            contains predictions indexed by name and the second element contains intermediate activations
+            index by name.
+
+        Raises:
+            ValueError: Occurs when something other than a tensor or dict of tensors is returned by the model
+            forward.
+        """
+
+        # We use features from init_global_model to compute the DEEP-MMD loss not the global_model
+        global_preds = self.global_model(input)
+        local_preds = self.model(input)
+        features = self.local_feature_extractor.get_extracted_features()
+        if self.deep_mmd_loss_weight != 0:
+            # Compute the features of the init_global_model
+            _ = self.init_global_model(input)
+            init_global_features = self.init_global_feature_extractor.get_extracted_features()
+            for key in init_global_features.keys():
+                features[" ".join(["init_global", key])] = init_global_features[key]
+
+        return {"global": global_preds, "local": local_preds}, features
+
+    def _maybe_checkpoint(self, loss: float, metrics: Dict[str, Scalar], checkpoint_mode: CheckpointMode) -> None:
+        # Hooks need to be removed before checkpointing the model
+        self.local_feature_extractor.remove_hooks()
+        super()._maybe_checkpoint(loss=loss, metrics=metrics, checkpoint_mode=checkpoint_mode)
+
+    def compute_evaluation_loss(
+        self,
+        preds: TorchPredType,
+        features: TorchFeatureType,
+        target: TorchTargetType,
+    ) -> EvaluationLosses:
+        """
+        Computes evaluation loss given predictions (and potentially features) of the model and ground truth data.
+        We also need to set the training flag of the deep_mmd_losses to False to ensure that the model is in eval mode.
+
+        Args:
+            preds (Dict[str, torch.Tensor]): Prediction(s) of the model(s) indexed by name. Anything stored
+                in preds will be used to compute metrics.
+            features: (Dict[str, torch.Tensor]): Feature(s) of the model(s) indexed by name.
+            target: (torch.Tensor): Ground truth data to evaluate predictions against.
+
+        Returns:
+            EvaluationLosses: an instance of EvaluationLosses containing checkpoint loss and additional losses
+                indexed by name.
+        """
+        for layer in self.flatten_feature_extraction_layers.keys():
+            self.deep_mmd_losses[layer].training = False
+        return super().compute_evaluation_loss(preds, features, target)
+
+    def compute_loss_and_additional_losses(
+        self, preds: TorchPredType, features: TorchFeatureType, target: TorchTargetType
+    ) -> Tuple[torch.Tensor, Dict[str, torch.Tensor]]:
+        """
+        Computes the loss and any additional losses given predictions of the model and ground truth data.
+
+        Args:
+            preds (TorchPredType): Prediction(s) of the model(s) indexed by name.
+            features (TorchFeatureType): Feature(s) of the model(s) indexed by name.
+            target (TorchTargetType): Ground truth data to evaluate predictions against.
+
+        Returns:
+            Tuple[torch.Tensor, Union[Dict[str, torch.Tensor], None]]; A tuple with:
+                - The tensor for the loss
+                - A dictionary of additional losses with their names and values, or None if
+                    there are no additional losses.
+        """
+        total_loss, additional_losses = super().compute_loss_and_additional_losses(preds, features, target)
+
+        if self.deep_mmd_loss_weight != 0:
+            # Compute DEEP-MMD loss
+            total_deep_mmd_loss = torch.tensor(0.0, device=self.device)
+            for layer in self.flatten_feature_extraction_layers.keys():
+                layer_deep_mmd_loss = self.deep_mmd_losses[layer](
+                    features[layer], features[" ".join(["init_global", layer])]
+                )
+                additional_losses["_".join(["deep_mmd_loss", layer])] = layer_deep_mmd_loss
+                total_deep_mmd_loss += layer_deep_mmd_loss
+            total_loss += self.deep_mmd_loss_weight * total_deep_mmd_loss
+            additional_losses["deep_mmd_loss_total"] = total_deep_mmd_loss
+        additional_losses["total_loss"] = total_loss
+
+        return total_loss, additional_losses
---
+++ fl4health/losses/deep_mmd_loss.py
@@ -0,0 +1,223 @@
+from typing import Optional, Tuple
+
+import numpy as np
+import torch
+
+
+class ModelLatentF(torch.nn.Module):
+    """Latent space for both domains."""
+
+    def __init__(self, x_in_dim: int, hidden_dim: int, x_out_dim: int):
+        """Init latent features."""
+        super().__init__()
+        self.latent = torch.nn.Sequential(
+            torch.nn.Linear(x_in_dim, hidden_dim, bias=True),
+            torch.nn.Softplus(),
+            torch.nn.Linear(hidden_dim, hidden_dim, bias=True),
+            torch.nn.Softplus(),
+            torch.nn.Linear(hidden_dim, hidden_dim, bias=True),
+            torch.nn.Softplus(),
+            torch.nn.Linear(hidden_dim, x_out_dim, bias=True),
+        )
+
+    def forward(self, input: torch.Tensor) -> torch.Tensor:
+        """Forward the LeNet."""
+        feature_latent_map = self.latent(input)
+        return feature_latent_map
+
+
+class DeepMmdLoss(torch.nn.Module):
+    def __init__(
+        self,
+        device: torch.device,
+        input_size: int,
+        lr: float = 0.001,
+        hidden_size: int = 10,
+        output_size: int = 50,
+        layer_name: Optional[str] = None,
+        training: bool = True,
+    ) -> None:
+        super().__init__()
+        self.device = device
+        self.lr = lr
+        self.layer_name = layer_name
+        self.training = training
+
+        # Initialize the model
+        self.featurizer = ModelLatentF(input_size, hidden_size, output_size).to(self.device)
+
+        # Initialize parameters
+        self.epsilonOPT: torch.Tensor = torch.log(torch.from_numpy(np.random.rand(1) * 10 ** (-10)).to(self.device))
+        self.epsilonOPT.requires_grad = self.training
+        self.sigmaOPT: torch.Tensor = torch.sqrt(torch.tensor(2 * 32 * 32, dtype=torch.float).to(self.device))
+        self.sigmaOPT.requires_grad = self.training
+        self.sigma0OPT: torch.Tensor = torch.sqrt(torch.tensor(0.005, dtype=torch.float).to(self.device))
+        self.sigma0OPT.requires_grad = self.training
+
+        # Initialize optimizers
+        self.optimizer_F = torch.optim.Adam(
+            list(self.featurizer.parameters()) + [self.epsilonOPT] + [self.sigmaOPT] + [self.sigma0OPT], lr=self.lr
+        )
+
+    def Pdist2(self, x: torch.Tensor, y: Optional[torch.Tensor]) -> torch.Tensor:
+        """compute the paired distance between x and y."""
+        x_norm = (x**2).sum(1).view(-1, 1)
+        if y is not None:
+            y_norm = (y**2).sum(1).view(1, -1)
+        else:
+            y = x
+            y_norm = x_norm.view(1, -1)
+        Pdist = x_norm + y_norm - 2.0 * torch.mm(x, torch.transpose(y, 0, 1))
+        Pdist[Pdist < 0] = 0
+        return Pdist
+
+    def h1_mean_var_gram(
+        self, Kx: torch.Tensor, Ky: torch.Tensor, Kxy: torch.Tensor, is_var_computed: bool, use_1sample_U: bool = True
+    ) -> Tuple[torch.Tensor, Optional[torch.Tensor], torch.Tensor]:
+        """compute value of MMD and std of MMD using kernel matrix."""
+        Kxxy = torch.cat((Kx, Kxy), 1)
+        Kyxy = torch.cat((Kxy.transpose(0, 1), Ky), 1)
+        Kxyxy = torch.cat((Kxxy, Kyxy), 0)
+        nx = Kx.shape[0]
+        ny = Ky.shape[0]
+        is_unbiased = True
+        if is_unbiased:
+            xx = torch.div((torch.sum(Kx) - torch.sum(torch.diag(Kx))), (nx * (nx - 1)))
+            yy = torch.div((torch.sum(Ky) - torch.sum(torch.diag(Ky))), (ny * (ny - 1)))
+            # one-sample U-statistic.
+            if use_1sample_U:
+                xy = torch.div((torch.sum(Kxy) - torch.sum(torch.diag(Kxy))), (nx * (ny - 1)))
+            else:
+                xy = torch.div(torch.sum(Kxy), (nx * ny))
+            mmd2 = xx - 2 * xy + yy
+        else:
+            xx = torch.div((torch.sum(Kx)), (nx * nx))
+            yy = torch.div((torch.sum(Ky)), (ny * ny))
+            # one-sample U-statistic.
+            if use_1sample_U:
+                xy = torch.div((torch.sum(Kxy)), (nx * ny))
+            else:
+                xy = torch.div(torch.sum(Kxy), (nx * ny))
+            mmd2 = xx - 2 * xy + yy
+        if not is_var_computed:
+            return mmd2, None, Kxyxy
+        hh = Kx + Ky - Kxy - Kxy.transpose(0, 1)
+        V1 = torch.dot(hh.sum(1) / ny, hh.sum(1) / ny) / ny
+        V2 = (hh).sum() / (nx) / nx
+        varEst = 4 * (V1 - V2**2)
+        return mmd2, varEst, Kxyxy
+
+    def MMDu(
+        self,
+        Fea: torch.Tensor,
+        len_s: int,
+        Fea_org: torch.Tensor,
+        sigma: torch.Tensor,
+        sigma0: torch.Tensor,
+        epsilon: torch.Tensor,
+        is_smooth: bool = True,
+        is_var_computed: bool = True,
+        use_1sample_U: bool = True,
+    ) -> Tuple[torch.Tensor, Optional[torch.Tensor], torch.Tensor]:
+        """compute value of deep-kernel MMD and std of deep-kernel MMD using merged data."""
+        X = Fea[0:len_s, :]  # fetch the sample 1 (features of deep networks)
+        Y = Fea[len_s:, :]  # fetch the sample 2 (features of deep networks)
+        X_org = Fea_org[0:len_s, :]  # fetch the original sample 1
+        Y_org = Fea_org[len_s:, :]  # fetch the original sample 2
+        L = 1  # generalized Gaussian (if L>1)
+        Dxx = self.Pdist2(X, X)
+        Dyy = self.Pdist2(Y, Y)
+        Dxy = self.Pdist2(X, Y)
+        Dxx_org = self.Pdist2(X_org, X_org)
+        Dyy_org = self.Pdist2(Y_org, Y_org)
+        Dxy_org = self.Pdist2(X_org, Y_org)
+        if is_smooth:
+            Kx = (1 - epsilon) * torch.exp(-((Dxx / sigma0) ** L) - Dxx_org / sigma) + epsilon * torch.exp(
+                -Dxx_org / sigma
+            )
+            Ky = (1 - epsilon) * torch.exp(-((Dyy / sigma0) ** L) - Dyy_org / sigma) + epsilon * torch.exp(
+                -Dyy_org / sigma
+            )
+            Kxy = (1 - epsilon) * torch.exp(-((Dxy / sigma0) ** L) - Dxy_org / sigma) + epsilon * torch.exp(
+                -Dxy_org / sigma
+            )
+        else:
+            Kx = torch.exp(-Dxx / sigma0)
+            Ky = torch.exp(-Dyy / sigma0)
+            Kxy = torch.exp(-Dxy / sigma0)
+
+        return self.h1_mean_var_gram(Kx, Ky, Kxy, is_var_computed, use_1sample_U)
+
+    def train_kernel(self, X: torch.Tensor, Y: torch.Tensor) -> None:
+        """Train the kernel."""
+
+        self.featurizer.train()
+        self.sigmaOPT.requires_grad = True
+        self.sigma0OPT.requires_grad = True
+        self.epsilonOPT.requires_grad = True
+
+        features = torch.cat([X, Y], 0)
+
+        # ------------------------------
+        #  Train deep network for MMD-D
+        # ------------------------------
+        # Initialize optimizer
+        self.optimizer_F.zero_grad()
+        # Compute output of deep network
+        model_output = self.featurizer(features)
+        # Compute epsilon, sigma and sigma_0
+        ep = torch.exp(self.epsilonOPT) / (1 + torch.exp(self.epsilonOPT))
+        sigma = self.sigmaOPT**2
+        sigma0_u = self.sigma0OPT**2
+        # Compute Compute J (STAT_u)
+        mmd_value_temp, mmd_var_temp, _ = self.MMDu(
+            Fea=model_output,
+            len_s=X.shape[0],
+            Fea_org=features.view(features.shape[0], -1),
+            sigma=sigma,
+            sigma0=sigma0_u,
+            epsilon=ep,
+        )
+        if mmd_var_temp is None:
+            raise AssertionError("Error: Variance of MMD is not computed. Please set is_var_computed=True.")
+        mmd_std_temp = torch.sqrt(mmd_var_temp + 10 ** (-8))
+        STAT_u = torch.div(-1 * mmd_value_temp, mmd_std_temp)
+        # Compute gradient
+        STAT_u.backward()
+        # Update weights using gradient descent
+        self.optimizer_F.step()
+        return
+
+    def compute_kernel(self, X: torch.Tensor, Y: torch.Tensor) -> torch.Tensor:
+        """Train the kernel."""
+
+        self.featurizer.eval()
+        self.sigmaOPT.requires_grad = False
+        self.sigma0OPT.requires_grad = False
+        self.epsilonOPT.requires_grad = False
+
+        features = torch.cat([X, Y], 0)
+
+        # Compute output of deep network
+        model_output = self.featurizer(features)
+        # Compute epsilon, sigma and sigma_0
+        ep = torch.exp(self.epsilonOPT) / (1 + torch.exp(self.epsilonOPT))
+        sigma = self.sigmaOPT**2
+        sigma0_u = self.sigma0OPT**2
+        # Compute Compute J (STAT_u)
+        mmd_value_temp, _, _ = self.MMDu(
+            Fea=model_output,
+            len_s=X.shape[0],
+            Fea_org=features.view(features.shape[0], -1),
+            sigma=sigma,
+            sigma0=sigma0_u,
+            epsilon=ep,
+        )
+
+        return mmd_value_temp
+
+    def forward(self, Xs: torch.Tensor, Xt: torch.Tensor) -> torch.Tensor:
+        if self.training:
+            self.train_kernel(Xs.clone().detach(), Xt.clone().detach())
+
+        return self.compute_kernel(Xs, Xt)
