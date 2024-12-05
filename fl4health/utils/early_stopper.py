6f20445ad8a8c4adb0501b8a7284ddec2327581b
sanaAyrml <ayromlous@gmail.com>
2024-12-05 18:03:13 -0500

    Separate early_stopper and snapshotter

--- fl4health/utils/early_stopper.py
+++ fl4health/utils/early_stopper.py
@@ -1,8 +1,6 @@
-import copy
-from abc import ABC, abstractmethod
 from logging import INFO
 from pathlib import Path
-from typing import Any, Callable, Generic, Optional, Type, TypeVar
+from typing import Any, Callable, Optional, Type
 
 import torch.nn as nn
 from flwr.common.logger import log
@@ -15,124 +13,18 @@ from fl4health.reporting.reports_manager import ReportsManager
 from fl4health.utils.logging import LoggingMode
 from fl4health.utils.losses import TrainingLosses
 from fl4health.utils.metrics import MetricManager
-
-T = TypeVar("T")
-
-
-class Snapshotter(ABC, Generic[T]):
-    def __init__(self, client: BasicClient) -> None:
-        self.client = client
-
-    def dict_wrap_attr(self, name: str, expected_type: Type[T]) -> dict[str, T]:
-        attribute = copy.deepcopy(getattr(self.client, name))
-        if isinstance(attribute, expected_type):
-            return {"None": attribute}
-        elif isinstance(attribute, dict):
-            for key, value in attribute.items():
-                if not isinstance(value, expected_type):
-                    raise ValueError(f"Uncompatible type of attribute {type(attribute)} for key {key}")
-            return attribute
-        else:
-            raise ValueError(f"Uncompatible type of attribute {type(attribute)}")
-
-    def save(self, name: str, expected_type: Type[T]) -> Any:
-        attribute = self.dict_wrap_attr(name, expected_type)
-        return self.save_attribute(attribute)
-
-    def load(self, ckpt: dict[str, Any], name: str, expected_type: Type[T]) -> None:
-        attribute = self.dict_wrap_attr(name, expected_type)
-        self.load_attribute(ckpt[name], attribute)
-        if list(attribute.keys()) == ["None"]:
-            setattr(self.client, name, attribute["None"])
-        else:
-            setattr(self.client, name, attribute)
-
-    @abstractmethod
-    def save_attribute(self, attribute: dict[str, T]) -> dict[str, Any]:
-        raise NotImplementedError
-
-    @abstractmethod
-    def load_attribute(self, attribute_ckpt: dict[str, Any], attribute: dict[str, T]) -> None:
-        raise NotImplementedError
+from fl4health.utils.snapshotter import (
+    LRSchedulerSnapshotter,
+    NumberSnapshotter,
+    OptimizerSnapshotter,
+    SerizableObjectSnapshotter,
+    Snapshotter,
+    T,
+    TorchModuleSnapshotter,
+)
 
 
-class OptimizerSnapshotter(Snapshotter[Optimizer]):
-
-    def save_attribute(self, attribute: dict[str, Optimizer]) -> dict[str, Any]:
-        """
-        Save the state of the optimizers (either single or dictionary of them).
-        """
-        output = {}
-        for key, optimizer in attribute.items():
-            output[key] = optimizer.state_dict()["state"]
-        return output
-
-    def load_attribute(self, attribute_ckpt: dict[str, Any], attribute: dict[str, Optimizer]) -> None:
-        for key, optimizer in attribute.items():
-            optimizer_state_dict = optimizer.state_dict()
-            optimizer_state_dict["state"] = attribute_ckpt[key]
-            optimizer.load_state_dict(optimizer_state_dict)
-
-
-class LRSchedulerSnapshotter(Snapshotter[_LRScheduler]):
-
-    def save_attribute(self, attribute: dict[str, _LRScheduler]) -> dict[str, Any]:
-        """
-        Save the state of the optimizers (either single or dictionary of them).
-        """
-        output = {}
-        for key, lr_scheduler in attribute.items():
-            output[key] = lr_scheduler.state_dict()
-        return output
-
-    def load_attribute(self, attribute_ckpt: dict[str, Any], attribute: dict[str, _LRScheduler]) -> None:
-        for key, lr_scheduler in attribute.items():
-            lr_scheduler.load_state_dict(attribute_ckpt[key])
-
-
-class TorchModuleSnapshotter(Snapshotter[nn.Module]):
-
-    def save_attribute(self, attribute: dict[str, nn.Module]) -> dict[str, Any]:
-        """
-        Save the state of the optimizers (either single or dictionary of them).
-        """
-        output = {}
-        for key, model in attribute.items():
-            output[key] = model.state_dict()
-        return output
-
-    def load_attribute(self, attribute_ckpt: dict[str, Any], attribute: dict[str, nn.Module]) -> None:
-        for key, model in attribute.items():
-            model.load_state_dict(attribute_ckpt[key])
-
-
-class SerizableObjectSnapshotter(Snapshotter[MetricManager | TrainingLosses | ReportsManager]):
-    def save_attribute(self, attribute: dict[str, MetricManager | TrainingLosses | ReportsManager]) -> dict[str, Any]:
-        """
-        Save the state of the optimizers (either single or dictionary of them).
-        """
-        return attribute
-
-    def load_attribute(
-        self, attribute_ckpt: dict[str, Any], attribute: dict[str, MetricManager | TrainingLosses | ReportsManager]
-    ) -> None:
-        for key in attribute:
-            attribute[key] = attribute_ckpt[key]
-
-
-class NumberSnapshotter(Snapshotter[int | float]):
-    def save_attribute(self, attribute: dict[str, int | float]) -> dict[str, Any]:
-        """
-        Save the state of the optimizers (either single or dictionary of them).
-        """
-        return attribute
-
-    def load_attribute(self, attribute_ckpt: dict[str, Any], attribute: dict[str, int | float]) -> None:
-        for key in attribute:
-            attribute[key] = attribute_ckpt[key]
-
-
-class EarlyStopper(ABC):
+class EarlyStopper:
     def __init__(
         self,
         client: BasicClient,
