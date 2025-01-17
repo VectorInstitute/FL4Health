aa7e73b8feb7601c3b62519ab14571a5cc2d5758
Val Andrei Fajardo <andrei@nerdai.io>
2025-01-17 13:15:16 -0500

    tol fixture

--- tests/smoke_tests/conftest.py
+++ tests/smoke_tests/conftest.py
@@ -2,6 +2,7 @@ import tempfile
 from typing import Iterator
 
 import pytest
+import torch
 
 
 @pytest.fixture(scope="session")
@@ -9,3 +10,11 @@ def test_checkpoint_dirname() -> Iterator[str]:
     tempdir = tempfile.TemporaryDirectory()
     yield tempdir.name
     tempdir.cleanup()
+
+
+@pytest.fixture(scope="session")
+def tolerance() -> float:
+    if torch.cuda.is_available():
+        return 0.005
+    else:
+        return 0.0005
--- tests/smoke_tests/test_smoke_tests.py
+++ tests/smoke_tests/test_smoke_tests.py
@@ -5,7 +5,7 @@ from .run_smoke_test import load_metrics_from_file, run_fault_tolerance_smoke_te
 
 @pytest.mark.smoketest
 @pytest.mark.asyncio()
-async def test_basic_server_client_cifar(test_checkpoint_dirname: str) -> None:
+async def test_basic_server_client_cifar(test_checkpoint_dirname: str, tolerance: float) -> None:
     await run_fault_tolerance_smoke_test(
         server_python_path="tests.smoke_tests.load_from_checkpoint_example.server",
         client_python_path="tests.smoke_tests.load_from_checkpoint_example.client",
@@ -16,34 +16,37 @@ async def test_basic_server_client_cifar(test_checkpoint_dirname: str) -> None:
         server_metrics=load_metrics_from_file("tests/smoke_tests/basic_server_metrics.json"),
         client_metrics=load_metrics_from_file("tests/smoke_tests/basic_client_metrics.json"),
         intermediate_checkpoint_dir=test_checkpoint_dirname,
+        tolerance=tolerance,
     )
 
 
 @pytest.mark.smoketest
 @pytest.mark.asyncio()
-async def test_nnunet_config_2d() -> None:
+async def test_nnunet_config_2d(tolerance: float) -> None:
     await run_smoke_test(  # By default will use Task04_Hippocampus Dataset
         server_python_path="examples.nnunet_example.server",
         client_python_path="examples.nnunet_example.client",
         config_path="tests/smoke_tests/nnunet_config_2d.yaml",
         dataset_path="examples/datasets/nnunet",
+        tolerance=tolerance,
     )
 
 
 @pytest.mark.smoketest
 @pytest.mark.asyncio()
-async def test_nnunet_config_3d() -> None:
+async def test_nnunet_config_3d(tolerance: float) -> None:
     await run_smoke_test(  # By default will use Task04_Hippocampus Dataset
         server_python_path="examples.nnunet_example.server",
         client_python_path="examples.nnunet_example.client",
         config_path="tests/smoke_tests/nnunet_config_3d.yaml",
         dataset_path="examples/datasets/nnunet",
+        tolerance=tolerance,
     )
 
 
 @pytest.mark.smoketest
 @pytest.mark.asyncio()
-async def test_scaffold() -> None:
+async def test_scaffold(tolerance: float) -> None:
     await run_smoke_test(
         server_python_path="examples.scaffold_example.server",
         client_python_path="examples.scaffold_example.client",
@@ -52,12 +55,13 @@ async def test_scaffold() -> None:
         seed=42,
         server_metrics=load_metrics_from_file("tests/smoke_tests/scaffold_server_metrics.json"),
         client_metrics=load_metrics_from_file("tests/smoke_tests/scaffold_client_metrics.json"),
+        tolerance=tolerance,
     )
 
 
 @pytest.mark.smoketest
 @pytest.mark.asyncio()
-async def test_apfl() -> None:
+async def test_apfl(tolerance: float) -> None:
     await run_smoke_test(
         server_python_path="examples.apfl_example.server",
         client_python_path="examples.apfl_example.client",
@@ -66,12 +70,13 @@ async def test_apfl() -> None:
         seed=42,
         server_metrics=load_metrics_from_file("tests/smoke_tests/apfl_server_metrics.json"),
         client_metrics=load_metrics_from_file("tests/smoke_tests/apfl_client_metrics.json"),
+        tolerance=tolerance,
     )
 
 
 @pytest.mark.smoketest
 @pytest.mark.asyncio()
-async def test_feddg_ga() -> None:
+async def test_feddg_ga(tolerance: float) -> None:
     await run_smoke_test(
         server_python_path="examples.feddg_ga_example.server",
         client_python_path="examples.feddg_ga_example.client",
@@ -80,81 +85,88 @@ async def test_feddg_ga() -> None:
         seed=42,
         server_metrics=load_metrics_from_file("tests/smoke_tests/feddg_ga_server_metrics.json"),
         client_metrics=load_metrics_from_file("tests/smoke_tests/feddg_ga_client_metrics.json"),
+        tolerance=tolerance,
     )
 
 
 @pytest.mark.smoketest
 @pytest.mark.asyncio()
-async def test_basic() -> None:
+async def test_basic(tolerance: float) -> None:
     await run_smoke_test(
         server_python_path="examples.basic_example.server",
         client_python_path="examples.basic_example.client",
         config_path="tests/smoke_tests/basic_config.yaml",
         dataset_path="examples/datasets/cifar_data/",
+        tolerance=tolerance,
     )
 
 
 @pytest.mark.smoketest
 @pytest.mark.asyncio()
-async def test_client_level_dp_cifar() -> None:
+async def test_client_level_dp_cifar(tolerance: float) -> None:
     await run_smoke_test(
         server_python_path="examples.dp_fed_examples.client_level_dp.server",
         client_python_path="examples.dp_fed_examples.client_level_dp.client",
         config_path="tests/smoke_tests/client_level_dp_config.yaml",
         dataset_path="examples/datasets/cifar_data/",
         skip_assert_client_fl_rounds=True,
+        tolerance=tolerance,
     )
 
 
 @pytest.mark.smoketest
 @pytest.mark.asyncio()
-async def test_client_level_dp_breast_cancer() -> None:
+async def test_client_level_dp_breast_cancer(tolerance: float) -> None:
     await run_smoke_test(
         server_python_path="examples.dp_fed_examples.client_level_dp_weighted.server",
         client_python_path="examples.dp_fed_examples.client_level_dp_weighted.client",
         config_path="tests/smoke_tests/client_level_dp_weighted_config.yaml",
         dataset_path="examples/datasets/breast_cancer_data/hospital_0.csv",
         skip_assert_client_fl_rounds=True,
+        tolerance=tolerance,
     )
 
 
 @pytest.mark.smoketest
 @pytest.mark.asyncio()
-async def test_instance_level_dp_cifar() -> None:
+async def test_instance_level_dp_cifar(tolerance: float) -> None:
     await run_smoke_test(
         server_python_path="examples.dp_fed_examples.instance_level_dp.server",
         client_python_path="examples.dp_fed_examples.instance_level_dp.client",
         config_path="tests/smoke_tests/instance_level_dp_config.yaml",
         dataset_path="examples/datasets/cifar_data/",
         skip_assert_client_fl_rounds=True,
+        tolerance=tolerance,
     )
 
 
 @pytest.mark.smoketest
 @pytest.mark.asyncio()
-async def test_dp_scaffold() -> None:
+async def test_dp_scaffold(tolerance: float) -> None:
     await run_smoke_test(
         server_python_path="examples.dp_scaffold_example.server",
         client_python_path="examples.dp_scaffold_example.client",
         config_path="tests/smoke_tests/dp_scaffold_config.yaml",
         dataset_path="examples/datasets/mnist_data/",
+        tolerance=tolerance,
     )
 
 
 @pytest.mark.smoketest
 @pytest.mark.asyncio()
-async def test_fedbn() -> None:
+async def test_fedbn(tolerance: float) -> None:
     await run_smoke_test(
         server_python_path="examples.fedbn_example.server",
         client_python_path="examples.fedbn_example.client",
         config_path="tests/smoke_tests/fedbn_config.yaml",
         dataset_path="examples/datasets/mnist_data/",
+        tolerance=tolerance,
     )
 
 
 @pytest.mark.smoketest
 @pytest.mark.asyncio()
-async def test_fed_eval() -> None:
+async def test_fed_eval(tolerance: float) -> None:
     await run_smoke_test(
         server_python_path="examples.federated_eval_example.server",
         client_python_path="examples.federated_eval_example.client",
@@ -162,28 +174,31 @@ async def test_fed_eval() -> None:
         dataset_path="examples/datasets/cifar_data/",
         checkpoint_path="examples/assets/fed_eval_example/best_checkpoint_fczjmljm.pkl",
         assert_evaluation_logs=True,
+        tolerance=tolerance,
     )
 
 
 @pytest.mark.smoketest
 @pytest.mark.asyncio()
-async def test_fedper_mnist() -> None:
+async def test_fedper_mnist(tolerance: float) -> None:
     await run_smoke_test(
         server_python_path="examples.fedper_example.server",
         client_python_path="examples.fedper_example.client",
         config_path="tests/smoke_tests/fedper_config.yaml",
         dataset_path="examples/datasets/mnist_data/",
+        tolerance=tolerance,
     )
 
 
 @pytest.mark.smoketest
 @pytest.mark.asyncio()
-async def test_fedper_cifar() -> None:
+async def test_fedper_cifar(tolerance: float) -> None:
     await run_smoke_test(
         server_python_path="examples.fedrep_example.server",
         client_python_path="examples.fedrep_example.client",
         config_path="tests/smoke_tests/fedrep_config.yaml",
         dataset_path="examples/datasets/cifar_data/",
+        tolerance=tolerance,
     )
 
 
@@ -200,88 +215,96 @@ async def test_ditto_mnist() -> None:
 
 @pytest.mark.smoketest
 @pytest.mark.asyncio()
-async def test_mr_mtl_mnist() -> None:
+async def test_mr_mtl_mnist(tolerance: float) -> None:
     await run_smoke_test(
         server_python_path="examples.mr_mtl_example.server",
         client_python_path="examples.mr_mtl_example.client",
         config_path="tests/smoke_tests/mr_mtl_config.yaml",
         dataset_path="examples/datasets/mnist_data/",
+        tolerance=tolerance,
     )
 
 
 @pytest.mark.smoketest
 @pytest.mark.asyncio()
-async def test_fenda() -> None:
+async def test_fenda(tolerance: float) -> None:
     await run_smoke_test(
         server_python_path="examples.fenda_example.server",
         client_python_path="examples.fenda_example.client",
         config_path="tests/smoke_tests/fenda_config.yaml",
         dataset_path="examples/datasets/mnist_data/",
+        tolerance=tolerance,
     )
 
 
 @pytest.mark.smoketest
 @pytest.mark.asyncio()
-async def test_fenda_ditto() -> None:
+async def test_fenda_ditto(tolerance: float) -> None:
     await run_smoke_test(
         server_python_path="examples.fenda_ditto_example.server",
         client_python_path="examples.fenda_ditto_example.client",
         config_path="tests/smoke_tests/fenda_ditto_config.yaml",
         dataset_path="examples/datasets/mnist_data/",
         checkpoint_path="examples/assets/",
+        tolerance=tolerance,
     )
 
 
 @pytest.mark.smoketest
 @pytest.mark.asyncio()
-async def test_perfcl() -> None:
+async def test_perfcl(tolerance: float) -> None:
     await run_smoke_test(
         server_python_path="examples.perfcl_example.server",
         client_python_path="examples.perfcl_example.client",
         config_path="tests/smoke_tests/perfcl_config.yaml",
         dataset_path="examples/datasets/mnist_data/",
+        tolerance=tolerance,
     )
 
 
 @pytest.mark.smoketest
 @pytest.mark.asyncio()
-async def test_fl_plus_local() -> None:
+async def test_fl_plus_local(tolerance: float) -> None:
     await run_smoke_test(
         server_python_path="examples.fl_plus_local_ft_example.server",
         client_python_path="examples.fl_plus_local_ft_example.client",
         config_path="tests/smoke_tests/fl_plus_local_ft_config.yaml",
         dataset_path="examples/datasets/cifar_data/",
+        tolerance=tolerance,
     )
 
 
 @pytest.mark.smoketest
 @pytest.mark.asyncio()
-async def test_moon() -> None:
+async def test_moon(tolerance: float) -> None:
     await run_smoke_test(
         server_python_path="examples.moon_example.server",
         client_python_path="examples.moon_example.client",
         config_path="tests/smoke_tests/moon_config.yaml",
         dataset_path="examples/datasets/mnist_data/",
+        tolerance=tolerance,
     )
 
 
 @pytest.mark.smoketest
 @pytest.mark.asyncio()
-async def test_ensemble() -> None:
+async def test_ensemble(tolerance: float) -> None:
     await run_smoke_test(
         server_python_path="examples.ensemble_example.server",
         client_python_path="examples.ensemble_example.client",
         config_path="tests/smoke_tests/ensemble_config.yaml",
         dataset_path="examples/datasets/mnist_data/",
+        tolerance=tolerance,
     )
 
 
 @pytest.mark.smoketest
 @pytest.mark.asyncio()
-async def test_flash() -> None:
+async def test_flash(tolerance: float) -> None:
     await run_smoke_test(
         server_python_path="examples.flash_example.server",
         client_python_path="examples.flash_example.client",
         config_path="tests/smoke_tests/flash_config.yaml",
         dataset_path="examples/datasets/cifar_data/",
+        tolerance=tolerance,
     )
