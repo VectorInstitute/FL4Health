a2ce17e50b454e3cc8ed36fbdff4fe0cb72c4255
David Emerson <43939939+emersodb@users.noreply.github.com>
2025-02-13 12:00:16 -0500

    Does this help my patch coverage enough?

--- tests/utils/config_test.py
+++ tests/utils/config_test.py
@@ -3,7 +3,7 @@ from pathlib import Path
 from flwr.common.typing import Config
 from pytest import raises
 
-from fl4health.utils.config import InvalidConfigError, check_config, load_config
+from fl4health.utils.config import InvalidConfigError, check_config, load_config, make_dict_with_epochs_or_steps
 
 
 def test_load_config() -> None:
@@ -39,3 +39,24 @@ def test_check_config() -> None:
 
     with raises(InvalidConfigError):
         check_config(config_4)
+
+
+def test_make_dict_with_epochs_or_steps() -> None:
+    # Should be an empty dict because both are none
+    d = make_dict_with_epochs_or_steps(local_epochs=None, local_steps=None)
+    assert len(d) == 0
+
+    # Should be a dict of length one with key local_epochs
+    d = make_dict_with_epochs_or_steps(local_epochs=5, local_steps=None)
+    assert len(d) == 1
+    assert d["local_epochs"] == 5
+
+    # Should be a dict of length one with key local_steps
+    d = make_dict_with_epochs_or_steps(local_epochs=None, local_steps=10)
+    assert len(d) == 1
+    assert d["local_steps"] == 10
+
+    # Should be a dict of length one with key local_epochs since that takes precedence
+    d = make_dict_with_epochs_or_steps(local_epochs=5, local_steps=10)
+    assert len(d) == 1
+    assert d["local_epochs"] == 5
