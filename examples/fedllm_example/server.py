77f8b98a9a781c82a851b8e5c0a648cca3920332
David Emerson <43939939+emersodb@users.noreply.github.com>
2025-03-03 16:02:14 -0500

    Some small fixes to get everything looking good checks-wise

--- examples/fedllm_example/server.py
+++ examples/fedllm_example/server.py
@@ -10,9 +10,8 @@ from flwr.server.client_manager import SimpleClientManager
 from flwr.server.strategy import FedAvg
 
 from examples.fedllm_example.model import get_model
-from examples.utils.functions import make_dict_with_epochs_or_steps
 from fl4health.servers.base_server import FlServer
-from fl4health.utils.config import load_config
+from fl4health.utils.config import load_config, make_dict_with_epochs_or_steps
 from fl4health.utils.metric_aggregation import evaluate_metrics_aggregation_fn, fit_metrics_aggregation_fn
 from fl4health.utils.parameter_extraction import get_all_model_parameters_peft
 from fl4health.utils.random import set_all_random_seeds
