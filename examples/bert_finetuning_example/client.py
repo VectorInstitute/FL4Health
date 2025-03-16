5e9b9130ab9d44742700a6c9ddea03d8ea6ee58a
fatemetkl <t.fateme@gmail.com>
2025-03-15 18:15:35 -0600

    BERT example

---
+++ examples/bert_finetuning_example/client.py
@@ -0,0 +1,126 @@
+import argparse
+from logging import INFO
+from pathlib import Path
+
+import flwr as fl
+import torch
+import torch.nn as nn
+from flwr.common.logger import log
+from flwr.common.typing import Config
+from torch.nn.modules.loss import _Loss
+from torch.optim import Optimizer
+from torch.utils.data import DataLoader
+from transformers import BertForSequenceClassification
+
+from fl4health.checkpointing.checkpointer import BestLossTorchModuleCheckpointer
+from fl4health.checkpointing.client_module import ClientCheckpointAndStateModule
+from fl4health.clients.basic_client import BasicClient, TorchInputType
+from fl4health.utils.config import narrow_dict_type
+from fl4health.utils.metrics import Accuracy
+from research.ag_news.client_data import construct_dataloaders
+
+
+class BertClient(BasicClient):
+    def __init__(
+        self,
+        data_path: Path,
+        metrics: list,
+        device: torch.device,
+        learning_rate: float,
+    ) -> None:
+        super().__init__(
+            data_path=data_path,
+            metrics=metrics,
+            device=device,
+        )
+        self.learning_rate: float = learning_rate
+
+    def get_model(self, config: Config) -> nn.Module:
+        num_classes = narrow_dict_type(config, "num_classes", int)
+        model = BertForSequenceClassification.from_pretrained("google-bert/bert-base-cased", num_labels=num_classes)
+        return model.to(self.device)
+
+    def get_data_loaders(self, config: Config) -> tuple[DataLoader, DataLoader]:
+        batch_size = narrow_dict_type(config, "batch_size", int)
+        sample_percentage = narrow_dict_type(config, "sample_percentage", float)
+        beta = narrow_dict_type(config, "beta", float)
+        train_loader, val_loader = construct_dataloaders(batch_size, sample_percentage, beta)
+        return train_loader, val_loader
+
+    def get_criterion(self, config: Config) -> _Loss:
+        return torch.nn.CrossEntropyLoss()
+
+    def get_optimizer(self, config: Config) -> Optimizer:
+        return torch.optim.AdamW(self.model.parameters(), lr=self.learning_rate, weight_decay=0.001)
+
+    def predict(self, input: TorchInputType) -> tuple[dict[str, torch.Tensor], dict[str, torch.Tensor]]:
+        # Here the predict method is overwritten in order
+        # to rename the key to match what comes with the hugging face datasets.
+        outputs, features = super().predict(input)
+        preds = {}
+        preds["prediction"] = outputs["logits"]
+        return preds, features
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="FL Client Main")
+    parser.add_argument("--dataset_path", action="store", type=str, help="Path to the local dataset", default="")
+    parser.add_argument(
+        "--server_address",
+        action="store",
+        type=str,
+        help="Server Address for the clients to communicate with the server through",
+        default="0.0.0.0:8080",
+    )
+    parser.add_argument(
+        "--client_number",
+        action="store",
+        type=int,
+        help="Number of the client. Used for checkpointing",
+        required=True,
+    )
+    parser.add_argument(
+        "--artifact_dir",
+        action="store",
+        type=str,
+        help="Path to save client artifacts such as logs and model checkpoints",
+        default="examples/bert_finetuning_example",
+    )
+    parser.add_argument(
+        "--learning_rate",
+        action="store",
+        type=float,
+        help="Learning rate used by the client",
+        default=0.00001,
+    )
+    args = parser.parse_args()
+
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    data_path = Path(args.dataset_path)
+
+    log(INFO, f"Device to be used: {device}")
+    log(INFO, f"Server Address: {args.server_address}")
+    log(INFO, f"Learning Rate: {args.learning_rate}")
+
+    # Checkpointing
+    checkpoint_dir = args.artifact_dir
+    checkpoint_name = f"client_{args.client_number}_best_model.pkl"
+    checkpoint_and_state_module = ClientCheckpointAndStateModule(
+        post_aggregation=BestLossTorchModuleCheckpointer(checkpoint_dir, checkpoint_name)
+    )
+
+    client = BertClient(
+        data_path,
+        [Accuracy("accuracy")],
+        device,
+        learning_rate=args.learning_rate,
+    )
+    # grpc_max_message_length is reset here so the entire model can be exchanged between the server and clients.
+    # Note that the server must be started with the same grpc_max_message_length. Otherwise communication
+    # of larger messages would still be blocked.
+    fl.client.start_client(
+        server_address=args.server_address,
+        client=client.to_client(),
+        grpc_max_message_length=2000000000,
+    )
+    client.shutdown()
--- examples/bert_finetuning_example/server.py
+++ examples/bert_finetuning_example/server.py
@@ -7,103 +7,96 @@ import flwr as fl
 from flwr.common.logger import log
 from flwr.common.typing import Config
 from flwr.server.client_manager import SimpleClientManager
+from flwr.server.strategy import FedAvg
 from transformers import BertForSequenceClassification
 
+from fl4health.checkpointing.checkpointer import BestLossTorchModuleCheckpointer, LatestTorchModuleCheckpointer
+from fl4health.checkpointing.server_module import BaseServerCheckpointAndStateModule
+from fl4health.parameter_exchange.full_exchanger import FullParameterExchanger
 from fl4health.servers.base_server import FlServer
-from fl4health.strategies.fedavg_dynamic_layer import FedAvgDynamicLayer
-from fl4health.utils.config import load_config
+from fl4health.utils.config import load_config, make_dict_with_epochs_or_steps
 from fl4health.utils.metric_aggregation import evaluate_metrics_aggregation_fn, fit_metrics_aggregation_fn
 from fl4health.utils.parameter_extraction import get_all_model_parameters
 
 
-def construct_config(
-    current_round: int,
-    local_steps: int,
+def fit_config(
     batch_size: int,
     num_classes: int,
-    normalize: bool,
-    filter_by_percentage: bool,
-    select_drift_more: bool,
+    n_server_rounds: int,
     sample_percentage: float,
     beta: float,
+    current_round: int,
+    local_epochs: int | None = None,
+    local_steps: int | None = None,
 ) -> Config:
     assert 0 < sample_percentage <= 1
     assert 0 < beta
     return {
-        "current_server_round": current_round,
-        "local_steps": local_steps,
+        **make_dict_with_epochs_or_steps(local_epochs, local_steps),
         "batch_size": batch_size,
         "num_classes": num_classes,
-        "normalize": normalize,
-        "filter_by_percentage": filter_by_percentage,
-        "select_drift_more": select_drift_more,
+        "n_server_rounds": n_server_rounds,
         "sample_percentage": sample_percentage,
         "beta": beta,
+        "current_server_round": current_round,
     }
-
-
-def fit_config(
-    local_steps: int,
-    batch_size: int,
-    num_classes: int,
-    normalize: bool,
-    filter_by_percentage: bool,
-    select_drift_more: bool,
-    sample_percentage: float,
-    beta: float,
-    current_round: int,
-) -> Config:
-    return construct_config(
-        current_round,
-        local_steps,
-        batch_size,
-        num_classes,
-        normalize,
-        filter_by_percentage,
-        select_drift_more,
-        sample_percentage,
-        beta,
-    )
 
 
 def main(config: dict[str, Any], server_address: str) -> None:
     # This function will be used to produce a config that is sent to each client to initialize their own environment
     fit_config_fn = partial(
         fit_config,
-        config["local_steps"],
         config["batch_size"],
         config["num_classes"],
-        config["normalize"],
-        config["filter_by_percentage"],
-        config["select_drift_more"],
+        config["n_server_rounds"],
         config["sample_percentage"],
         config["beta"],
+        local_epochs=config.get("local_epochs"),
+        local_steps=config.get("local_steps"),
     )
 
+    # Initializing the model on the server side
     initial_model = BertForSequenceClassification.from_pretrained(
         "google-bert/bert-base-cased", num_labels=config["num_classes"]
     )
+    # To facilitate checkpointing
+    parameter_exchanger = FullParameterExchanger()
+    checkpointers = [
+        BestLossTorchModuleCheckpointer(config["checkpoint_path"], "best_model.pkl"),
+        LatestTorchModuleCheckpointer(config["checkpoint_path"], "latest_model.pkl"),
+    ]
+    checkpoint_and_state_module = BaseServerCheckpointAndStateModule(
+        model=initial_model, parameter_exchanger=parameter_exchanger, model_checkpointers=checkpointers
+    )
 
-    # Since clients can send different tensors to the server, we perform weighted averaging separately on each tensor.
-    strategy = FedAvgDynamicLayer(
+    # Server performs simple FedAveraging as its server-side optimization strategy
+    strategy = FedAvg(
+        min_fit_clients=config["n_clients"],
+        min_evaluate_clients=config["n_clients"],
         # Server waits for min_available_clients before starting FL rounds
         min_available_clients=config["n_clients"],
         on_fit_config_fn=fit_config_fn,
+        # We use the same fit config function, as nothing changes for eval
         on_evaluate_config_fn=fit_config_fn,
         fit_metrics_aggregation_fn=fit_metrics_aggregation_fn,
         evaluate_metrics_aggregation_fn=evaluate_metrics_aggregation_fn,
         initial_parameters=get_all_model_parameters(initial_model),
     )
 
-    client_manager = SimpleClientManager()
-    server = FlServer(client_manager=client_manager, fl_config=config, strategy=strategy)
+    server = FlServer(
+        client_manager=SimpleClientManager(),
+        fl_config=config,
+        strategy=strategy,
+        checkpoint_and_state_module=checkpoint_and_state_module,
+        accept_failures=False,
+    )
 
     # grpc_max_message_length is reset here so the entire model can be exchanged between the server and clients.
     fl.server.start_server(
         server_address=server_address,
         config=fl.server.ServerConfig(num_rounds=config["n_server_rounds"]),
         server=server,
-        grpc_max_message_length=2000000000,
+        grpc_max_message_length=1600000000,
     )
 
 
