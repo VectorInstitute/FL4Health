8cbf045253c5a5b9a4f5e04e191ede51ac3fc8b0
Marcelo Lotif <marcelo.lotif@vectorinstitute.ai>
2025-04-17 14:37:08 -0400

    Revert "Revert "WIP training working with superlinks and supernodes""
    This reverts commit ec8808dcf648ce2af4f7780ba9670164cd012024.

--- examples/basic-example/basic_example/client.py
+++ examples/basic-example/basic_example/client.py
@@ -1,15 +1,15 @@
-import argparse
 from pathlib import Path
 
-import flwr as fl
 import torch
 import torch.nn as nn
+from basic_example.model import Net
+from flwr.client import Client, ClientApp
+from flwr.common import Context
 from flwr.common.typing import Config
 from torch.nn.modules.loss import _Loss
 from torch.optim import Optimizer
 from torch.utils.data import DataLoader
 
-from examples.models.cnn_model import Net
 from fl4health.clients.basic_client import BasicClient
 from fl4health.utils.config import narrow_dict_type
 from fl4health.utils.load_data import load_cifar10_data, load_cifar10_test_data
@@ -37,13 +37,12 @@ class CifarClient(BasicClient):
         return Net().to(self.device)
 
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="FL Client Main")
-    parser.add_argument("--dataset_path", action="store", type=str, help="Path to the local dataset")
-    args = parser.parse_args()
+def client_fn(context: Context) -> Client:
+    dataset_path = context.node_config["dataset_path"]
+    assert isinstance(dataset_path, str), "dataset_path must be a string"
 
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    data_path = Path(args.dataset_path)
-    client = CifarClient(data_path, [Accuracy("accuracy")], device)
-    fl.client.start_client(server_address="0.0.0.0:8080", client=client.to_client())
-    client.shutdown()
+    return CifarClient(Path(dataset_path), [Accuracy("accuracy")], device).to_client()
+
+
+app = ClientApp(client_fn=client_fn)
