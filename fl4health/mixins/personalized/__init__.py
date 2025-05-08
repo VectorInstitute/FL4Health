c811fbcba7f1e28bf279ffb095f1b3b26f233107
Val Andrei Fajardo <andrei@nerdai.io>
2025-05-08 10:10:35 -0400

    reimports and public api for mixins.personalized

---
+++ fl4health/mixins/personalized/__init__.py
@@ -0,0 +1,32 @@
+from enum import Enum
+
+from fl4health.clients.basic_client import BasicClient
+from fl4health.mixins.personalized.ditto import DittoPersonalizedMixin
+
+
+class PersonalizedModes(str, Enum):
+    DITTO = "ditto"
+
+
+PersonalizedMixinRegistry = {"ditto": DittoPersonalizedMixin}
+
+
+def make_it_personal(client_base_type: type[BasicClient], mode: PersonalizedModes) -> type[BasicClient]:
+    if mode == "ditto":
+
+        return type(
+            f"Ditto{client_base_type.__name__}",
+            (
+                PersonalizedMixinRegistry[mode],
+                client_base_type,
+            ),
+            {
+                # Special flag to bypass validation
+                "_dynamically_created": True
+            },
+        )
+    else:
+        raise ValueError("Unrecognized personalized mode.")
+
+
+__all__ = ["DittoPersonalizedMixin", "PersonalizedModes", "PersonalizedMixinRegistry", "make_it_personal"]
