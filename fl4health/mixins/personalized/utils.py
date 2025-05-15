9e82518fb9f2c0c6615fee69f4e1bf6e4c63b497
Val Andrei Fajardo <andrei@nerdai.io>
2025-05-14 23:19:43 -0400

    tests for ensure_protocol_compliance and add unit tests

--- fl4health/mixins/personalized/utils.py
+++ fl4health/mixins/personalized/utils.py
@@ -7,7 +7,7 @@ from fl4health.clients.basic_client import BasicClient
 
 
 @wrapt.decorator
-def ensure_protocol_compliance(func: Callable, instance: Any | None, *args: Any, **kwargs: Any) -> None:
+def ensure_protocol_compliance(func: Callable, instance: Any | None, args: Any, kwargs: Any) -> None:
     # validate self is a BasicClient
     self = instance
     if not isinstance(self, BasicClient):
