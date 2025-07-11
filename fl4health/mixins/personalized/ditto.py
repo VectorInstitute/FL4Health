3c2860743562565541203c8c204db500ef8f0053
Val Andrei Fajardo <andrei@nerdai.io>
2025-07-11 10:05:56 -0400

    lint

--- fl4health/mixins/personalized/ditto.py
+++ fl4health/mixins/personalized/ditto.py
@@ -37,9 +37,7 @@ class DittoPersonalizedProtocol(AdaptiveDriftConstrainedProtocol, Protocol):
     def get_global_model(self, config: Config) -> nn.Module:
         pass  # pragma: no cover
 
-    def _copy_optimizer_with_new_params(
-        self, original_optimizer: Optimizer
-    ) -> Optimizer:
+    def _copy_optimizer_with_new_params(self, original_optimizer: Optimizer) -> Optimizer:
         pass  # pragma: no cover
 
     def set_initial_global_tensors(self) -> None:
@@ -79,9 +77,7 @@ class DittoPersonalizedMixin(AdaptiveDriftConstrainedMixin):
             super().__init__()
 
         if not isinstance(self, FlexibleClientProtocolPreSetup):
-            raise RuntimeError(
-                "This object needs to satisfy `FlexibleClientProtocolPreSetup`."
-            )  # pragma: no cover
+            raise RuntimeError("This object needs to satisfy `FlexibleClientProtocolPreSetup`.")  # pragma: no cover
 
     def __init_subclass__(cls, **kwargs: Any) -> None:
         """This method is called when a class inherits from AdaptiveMixin."""
@@ -132,9 +128,7 @@ class DittoPersonalizedMixin(AdaptiveDriftConstrainedMixin):
         """
         return ["local", "global"]
 
-    def _copy_optimizer_with_new_params(
-        self: DittoPersonalizedProtocol, original_optimizer: Optimizer
-    ) -> Optimizer:
+    def _copy_optimizer_with_new_params(self: DittoPersonalizedProtocol, original_optimizer: Optimizer) -> Optimizer:
         """
         Helper method to make a copy of the original optimizer for the global model.
 
@@ -164,13 +158,9 @@ class DittoPersonalizedMixin(AdaptiveDriftConstrainedMixin):
                     "Unable to get the original `lr` for the global optimizer, falling back to `1e-3`.",
                 )
 
-        optimizer_kwargs = {
-            k: v for k, v in param_group.items() if k not in ("params", "initial_lr")
-        }
+        optimizer_kwargs = {k: v for k, v in param_group.items() if k not in ("params", "initial_lr")}
         assert self.global_model is not None
-        global_optimizer = optim_class(
-            self.global_model.parameters(), **optimizer_kwargs
-        )
+        global_optimizer = optim_class(self.global_model.parameters(), **optimizer_kwargs)
 
         # maintain initial_lr for schedulers
         for param_group in global_optimizer.param_groups:
@@ -195,9 +185,7 @@ class DittoPersonalizedMixin(AdaptiveDriftConstrainedMixin):
         return model_copy.to(self.device)
 
     @ensure_protocol_compliance
-    def get_optimizer(
-        self: DittoPersonalizedProtocol, config: Config
-    ) -> dict[str, Optimizer]:
+    def get_optimizer(self: DittoPersonalizedProtocol, config: Config) -> dict[str, Optimizer]:
         """
         Returns a dictionary with global and local optimizers with string keys "global" and "local" respectively.
 
@@ -206,9 +194,7 @@ class DittoPersonalizedMixin(AdaptiveDriftConstrainedMixin):
         """
         if self.global_model is None:
             # try set it here
-            self.global_model = self.get_global_model(
-                config
-            )  # is this the same config?
+            self.global_model = self.get_global_model(config)  # is this the same config?
             log(
                 INFO,
                 f"global model set: {type(self.global_model).__name__} within `get_optimizer`",
@@ -218,9 +204,7 @@ class DittoPersonalizedMixin(AdaptiveDriftConstrainedMixin):
         optimizer = super().get_optimizer(config=config)  # type: ignore[safe-super]
         if isinstance(optimizer, dict):
             try:
-                original_optimizer = next(
-                    el for el in optimizer.values() if isinstance(el, Optimizer)
-                )
+                original_optimizer = next(el for el in optimizer.values() if isinstance(el, Optimizer))
             except StopIteration as e:
                 log(ERROR, "Unable to find an ~torch.optim.Optimizer object.")
                 raise e
@@ -241,9 +225,7 @@ class DittoPersonalizedMixin(AdaptiveDriftConstrainedMixin):
             config (Config): The config from the server.
         """
         optimizers = self.get_optimizer(config)
-        assert isinstance(optimizers, dict) and set(self.optimizer_keys) == set(
-            optimizers.keys()
-        )
+        assert isinstance(optimizers, dict) and set(self.optimizer_keys) == set(optimizers.keys())
         self.optimizers = optimizers
 
     @ensure_protocol_compliance
@@ -285,16 +267,12 @@ class DittoPersonalizedMixin(AdaptiveDriftConstrainedMixin):
         # NOTE: the global model weights are sent to the server here.
         if self.global_model is None:
             raise ValueError("Unable to get parameters with unset global model.")
-        global_model_weights = self.parameter_exchanger.push_parameters(
-            self.global_model, config=config
-        )
+        global_model_weights = self.parameter_exchanger.push_parameters(self.global_model, config=config)
 
         # Weights and training loss sent to server for aggregation
         # Training loss sent because server will decide to increase or decrease the penalty weight, if adaptivity
         # is turned on
-        packed_params = self.parameter_exchanger.pack_parameters(
-            global_model_weights, self.loss_for_adaptation
-        )
+        packed_params = self.parameter_exchanger.pack_parameters(global_model_weights, self.loss_for_adaptation)
         log(INFO, "Successfully packed parameters of global model")
         return packed_params
 
@@ -323,17 +301,9 @@ class DittoPersonalizedMixin(AdaptiveDriftConstrainedMixin):
                 the server.
         """
         # Make sure that the proper components exist.
-        assert (
-            self.global_model is not None
-            and self.model is not None
-            and self.parameter_exchanger is not None
-        )
-        server_model_state, self.drift_penalty_weight = (
-            self.parameter_exchanger.unpack_parameters(parameters)
-        )
-        log(
-            INFO, f"Lambda weight received from the server: {self.drift_penalty_weight}"
-        )
+        assert self.global_model is not None and self.model is not None and self.parameter_exchanger is not None
+        server_model_state, self.drift_penalty_weight = self.parameter_exchanger.unpack_parameters(parameters)
+        log(INFO, f"Lambda weight received from the server: {self.drift_penalty_weight}")
 
         current_server_round = narrow_dict_type(config, "current_server_round", int)
         if current_server_round == 1 and fitting_round:
@@ -345,13 +315,9 @@ class DittoPersonalizedMixin(AdaptiveDriftConstrainedMixin):
         else:
             # Route the parameters to the GLOBAL model in Ditto after the initial stage
             log(INFO, "Setting the global model weights")
-            self.parameter_exchanger.pull_parameters(
-                server_model_state, self.global_model, config
-            )
+            self.parameter_exchanger.pull_parameters(server_model_state, self.global_model, config)
 
-    def initialize_all_model_weights(
-        self: DittoPersonalizedProtocol, parameters: NDArrays, config: Config
-    ) -> None:
+    def initialize_all_model_weights(self: DittoPersonalizedProtocol, parameters: NDArrays, config: Config) -> None:
         """
         If this is the first time we're initializing the model weights, we initialize both the global and the local
         weights together.
@@ -362,9 +328,7 @@ class DittoPersonalizedMixin(AdaptiveDriftConstrainedMixin):
         """
         parameter_exchanger = cast(FullParameterExchanger, self.parameter_exchanger)
         parameter_exchanger.pull_parameters(parameters, self.model, config)
-        parameter_exchanger.pull_parameters(
-            parameters, self.safe_global_model(), config
-        )
+        parameter_exchanger.pull_parameters(parameters, self.safe_global_model(), config)
 
     def set_initial_global_tensors(self: DittoPersonalizedProtocol) -> None:
         """
@@ -372,14 +336,11 @@ class DittoPersonalizedMixin(AdaptiveDriftConstrainedMixin):
         respect to the tensors. These are used to form the Ditto local update penalty term.
         """
         self.drift_penalty_tensors = [
-            initial_layer_weights.detach().clone()
-            for initial_layer_weights in self.safe_global_model().parameters()
+            initial_layer_weights.detach().clone() for initial_layer_weights in self.safe_global_model().parameters()
         ]
 
     @ensure_protocol_compliance
-    def update_before_train(
-        self: DittoPersonalizedProtocol, current_server_round: int
-    ) -> None:
+    def update_before_train(self: DittoPersonalizedProtocol, current_server_round: int) -> None:
         """
         Procedures that should occur before proceeding with the training loops for the models. In this case, we
         save the global models parameters to be used in constraining training of the local model.
@@ -419,12 +380,8 @@ class DittoPersonalizedMixin(AdaptiveDriftConstrainedMixin):
             self.safe_global_model(), self.optimizers["global"], input, target
         )
         # local
-        local_losses, local_preds = self._compute_preds_and_losses(
-            self.model, self.optimizers["local"], input, target
-        )
-        local_loss_clone = local_losses.backward[
-            "backward"
-        ].clone()  # need a clone for later
+        local_losses, local_preds = self._compute_preds_and_losses(self.model, self.optimizers["local"], input, target)
+        local_loss_clone = local_losses.backward["backward"].clone()  # need a clone for later
 
         # take step global
         global_losses = self._apply_backwards_on_losses_and_take_step(
@@ -432,9 +389,7 @@ class DittoPersonalizedMixin(AdaptiveDriftConstrainedMixin):
         )
         # take step local
         penalty_loss = self.compute_penalty_loss()
-        local_losses.backward["backward"] = (
-            local_losses.backward["backward"] + penalty_loss
-        )
+        local_losses.backward["backward"] = local_losses.backward["backward"] + penalty_loss
         local_losses = self._apply_backwards_on_losses_and_take_step(
             self.model, self.optimizers["local"], local_losses
         )
@@ -449,9 +404,7 @@ class DittoPersonalizedMixin(AdaptiveDriftConstrainedMixin):
         local_losses.additional_losses = additional_losses
 
         # combined preds
-        if isinstance(global_preds, torch.Tensor) and isinstance(
-            local_preds, torch.Tensor
-        ):
+        if isinstance(global_preds, torch.Tensor) and isinstance(local_preds, torch.Tensor):
             combined_preds = {"global": global_preds, "local": local_preds}
         elif isinstance(global_preds, dict) and isinstance(local_preds, dict):
             combined_preds = {f"global-{k}": v for k, v in global_preds.items()}
@@ -463,9 +416,7 @@ class DittoPersonalizedMixin(AdaptiveDriftConstrainedMixin):
         self: DittoPersonalizedProtocol, input: TorchInputType, target: TorchTargetType
     ) -> tuple[EvaluationLosses, TorchPredType]:
         # global
-        global_losses, global_preds = self._val_step_with_model(
-            self.safe_global_model(), input, target
-        )
+        global_losses, global_preds = self._val_step_with_model(self.safe_global_model(), input, target)
         # local
         local_losses, local_preds = self._val_step_with_model(self.model, input, target)
 
@@ -519,9 +470,5 @@ class DittoPersonalizedMixin(AdaptiveDriftConstrainedMixin):
             indexed by name.
         """
         # Check that both models are in eval mode
-        assert (
-            self.global_model is not None
-            and not self.global_model.training
-            and not self.model.training
-        )
+        assert self.global_model is not None and not self.global_model.training and not self.model.training
         return super().compute_evaluation_loss(preds, features, target)  # type: ignore[safe-super]
--- fl4health/servers/base_server.py
+++ fl4health/servers/base_server.py
@@ -138,13 +138,9 @@ class FlServer(Server):
             round_metrics = {}
             for metric, vals in history.metrics_centralized.items():
                 round_metrics.update({metric: vals[round][1]})
-            self.reports_manager.report(
-                {"eval_round_metrics_centralized": round_metrics}, round + 1
-            )
+            self.reports_manager.report({"eval_round_metrics_centralized": round_metrics}, round + 1)
 
-    def fit_with_per_round_checkpointing(
-        self, num_rounds: int, timeout: float | None
-    ) -> tuple[History, float]:
+    def fit_with_per_round_checkpointing(self, num_rounds: int, timeout: float | None) -> tuple[History, float]:
         """
         Runs federated learning for a number of rounds. Heavily based on the fit method from the base
         server provided by flower (``flwr.server.server.Server``) except that it is resilient to preemptions.
@@ -196,14 +192,10 @@ class FlServer(Server):
                 parameters_prime, fit_metrics, _ = res_fit  # fit_metrics_aggregated
                 if parameters_prime:
                     self.parameters = parameters_prime
-                self.history.add_metrics_distributed_fit(
-                    server_round=self.current_round, metrics=fit_metrics
-                )
+                self.history.add_metrics_distributed_fit(server_round=self.current_round, metrics=fit_metrics)
 
             # Evaluate model using strategy implementation
-            res_cen = self.strategy.evaluate(
-                self.current_round, parameters=self.parameters
-            )
+            res_cen = self.strategy.evaluate(self.current_round, parameters=self.parameters)
             if res_cen is not None:
                 loss_cen, metrics_cen = res_cen
                 log(
@@ -214,26 +206,16 @@ class FlServer(Server):
                     metrics_cen,
                     (datetime.datetime.now() - start_time).total_seconds(),
                 )
-                self.history.add_loss_centralized(
-                    server_round=self.current_round, loss=loss_cen
-                )
-                self.history.add_metrics_centralized(
-                    server_round=self.current_round, metrics=metrics_cen
-                )
+                self.history.add_loss_centralized(server_round=self.current_round, loss=loss_cen)
+                self.history.add_metrics_centralized(server_round=self.current_round, metrics=metrics_cen)
 
             # Evaluate model on a sample of available clients
-            res_fed = self.evaluate_round(
-                server_round=self.current_round, timeout=timeout
-            )
+            res_fed = self.evaluate_round(server_round=self.current_round, timeout=timeout)
             if res_fed:
                 loss_fed, evaluate_metrics_fed, _ = res_fed
                 if loss_fed:
-                    self.history.add_loss_distributed(
-                        server_round=self.current_round, loss=loss_fed
-                    )
-                    self.history.add_metrics_distributed(
-                        server_round=self.current_round, metrics=evaluate_metrics_fed
-                    )
+                    self.history.add_loss_distributed(server_round=self.current_round, loss=loss_fed)
+                    self.history.add_metrics_distributed(server_round=self.current_round, metrics=evaluate_metrics_fed)
 
             self.current_round += 1
 
@@ -274,9 +256,7 @@ class FlServer(Server):
         self.update_before_fit(num_rounds, timeout)
 
         if self.checkpoint_and_state_module.state_checkpointer is not None:
-            history, elapsed_time = self.fit_with_per_round_checkpointing(
-                num_rounds, timeout
-            )
+            history, elapsed_time = self.fit_with_per_round_checkpointing(num_rounds, timeout)
         else:
             history, elapsed_time = super().fit(num_rounds, timeout)
         end_time = datetime.datetime.now()
@@ -324,9 +304,7 @@ class FlServer(Server):
             {
                 "fit_round_start": str(round_start),
                 "fit_round_end": str(round_end),
-                "fit_round_time_elapsed": round(
-                    (round_end - round_start).total_seconds()
-                ),
+                "fit_round_time_elapsed": round((round_end - round_start).total_seconds()),
             },
             server_round,
         )
@@ -362,9 +340,7 @@ class FlServer(Server):
         # the StrategyWithPolling ABC and implement a configure_poll function
         log(INFO, "Polling Clients for sample counts")
         assert isinstance(self.strategy, StrategyWithPolling)
-        client_instructions = self.strategy.configure_poll(
-            server_round=1, client_manager=self._client_manager
-        )
+        client_instructions = self.strategy.configure_poll(server_round=1, client_manager=self._client_manager)
         results, _ = poll_clients(
             client_instructions=client_instructions,
             max_workers=self.max_workers,
@@ -372,8 +348,7 @@ class FlServer(Server):
         )
 
         sample_counts: list[int] = [
-            int(get_properties_res.properties["num_train_samples"])
-            for (_, get_properties_res) in results
+            int(get_properties_res.properties["num_train_samples"]) for (_, get_properties_res) in results
         ]
         log(INFO, f"Polling complete: Retrieved {len(sample_counts)} sample counts")
 
@@ -413,28 +388,20 @@ class FlServer(Server):
                 self._terminate_after_unacceptable_failures(timeout)
 
             if loss_aggregated:
-                self._maybe_checkpoint(
-                    loss_aggregated, metrics_aggregated, server_round
-                )
+                self._maybe_checkpoint(loss_aggregated, metrics_aggregated, server_round)
                 # Report evaluation results
                 report_data = {
                     "val - loss - aggregated": loss_aggregated,
                     "round": server_round,
                     "eval_round_start": str(start_time),
                     "eval_round_end": str(end_time),
-                    "eval_round_time_elapsed": round(
-                        (end_time - start_time).total_seconds()
-                    ),
+                    "eval_round_time_elapsed": round((end_time - start_time).total_seconds()),
                 }
 
                 if self.fl_config.get("local_epochs", None) is not None:
-                    report_data["fit_epoch"] = (
-                        server_round * self.fl_config["local_epochs"]
-                    )
+                    report_data["fit_epoch"] = server_round * self.fl_config["local_epochs"]
                 elif self.fl_config.get("local_steps", None) is not None:
-                    report_data["fit_step"] = (
-                        server_round * self.fl_config["local_steps"]
-                    )
+                    report_data["fit_step"] = server_round * self.fl_config["local_steps"]
                 self.reports_manager.report(report_data, server_round)
                 if len(metrics_aggregated) > 0:
                     self.reports_manager.report(
@@ -445,9 +412,7 @@ class FlServer(Server):
         return eval_round_results
 
     def _log_fl_config(self) -> None:
-        log(INFO, "FL Configuration:") if self.fl_config else log(
-            INFO, "FL Config is Empty"
-        )
+        log(INFO, "FL Configuration:") if self.fl_config else log(INFO, "FL Config is Empty")
         for config_key, config_value in self.fl_config.items():
             if not isinstance(config_value, bytes):
                 log(INFO, f"Key: {config_key} Value: {config_value!r}")
@@ -521,14 +486,10 @@ class FlServer(Server):
             metrics_aggregated (dict[str, Scalar]): aggregated metrics from each of the clients for checkpointing
             server_round (int): What round of federated training we're on. This is just for logging purposes.
         """
-        self.checkpoint_and_state_module.maybe_checkpoint(
-            self.parameters, loss_aggregated, metrics_aggregated
-        )
+        self.checkpoint_and_state_module.maybe_checkpoint(self.parameters, loss_aggregated, metrics_aggregated)
 
     @override
-    def _get_initial_parameters(
-        self, server_round: int, timeout: float | None
-    ) -> Parameters:
+    def _get_initial_parameters(self, server_round: int, timeout: float | None) -> Parameters:
         """
         Get initial parameters from one of the available clients. This function is the same as the parent function
         in the flower server class except that we make use of the ``on_parameter_initialization_config_fn`` to provide
@@ -540,9 +501,7 @@ class FlServer(Server):
         flower: https://github.com/adap/flower/issues/3770.
         """
         # Server-side parameter initialization
-        parameters: Parameters | None = self.strategy.initialize_parameters(
-            client_manager=self._client_manager
-        )
+        parameters: Parameters | None = self.strategy.initialize_parameters(client_manager=self._client_manager)
         if parameters is not None:
             log(INFO, "Using initial global parameters provided by strategy")
             return parameters
@@ -566,12 +525,8 @@ class FlServer(Server):
             # An empty configuration is the default for Flower servers
             ins = GetParametersIns(config={})
         else:
-            ins = GetParametersIns(
-                config=self.on_init_parameters_config_fn(server_round)
-            )
-        get_parameters_res = random_client.get_parameters(
-            ins=ins, timeout=timeout, group_id=server_round
-        )
+            ins = GetParametersIns(config=self.on_init_parameters_config_fn(server_round))
+        get_parameters_res = random_client.get_parameters(ins=ins, timeout=timeout, group_id=server_round)
         if get_parameters_res.status.code == Code.OK:
             log(INFO, "Received initial parameters from one random client")
         else:
@@ -589,43 +544,28 @@ class FlServer(Server):
 
     def _unpack_metrics(
         self, results: list[tuple[ClientProxy, EvaluateRes]]
-    ) -> tuple[
-        list[tuple[ClientProxy, EvaluateRes]], list[tuple[ClientProxy, EvaluateRes]]
-    ]:
+    ) -> tuple[list[tuple[ClientProxy, EvaluateRes]], list[tuple[ClientProxy, EvaluateRes]]]:
         val_results = []
         test_results = []
 
         for client_proxy, eval_res in results:
             val_metrics = {
-                k: v
-                for k, v in eval_res.metrics.items()
-                if not k.startswith(MetricPrefix.TEST_PREFIX.value)
+                k: v for k, v in eval_res.metrics.items() if not k.startswith(MetricPrefix.TEST_PREFIX.value)
             }
-            test_metrics = {
-                k: v
-                for k, v in eval_res.metrics.items()
-                if k.startswith(MetricPrefix.TEST_PREFIX.value)
-            }
+            test_metrics = {k: v for k, v in eval_res.metrics.items() if k.startswith(MetricPrefix.TEST_PREFIX.value)}
 
             if len(test_metrics) > 0:
-                assert (
-                    TEST_LOSS_KEY in test_metrics
-                    and TEST_NUM_EXAMPLES_KEY in test_metrics
-                ), (
+                assert TEST_LOSS_KEY in test_metrics and TEST_NUM_EXAMPLES_KEY in test_metrics, (
                     f"'{TEST_NUM_EXAMPLES_KEY}' and '{TEST_LOSS_KEY}' keys must be present in "
                     "test_metrics dictionary for aggregation"
                 )
                 # Remove loss and num_examples from test_metrics if they exist
                 test_loss = float(test_metrics.pop(TEST_LOSS_KEY))
                 test_num_examples = int(test_metrics.pop(TEST_NUM_EXAMPLES_KEY))
-                test_eval_res = EvaluateRes(
-                    eval_res.status, test_loss, test_num_examples, test_metrics
-                )
+                test_eval_res = EvaluateRes(eval_res.status, test_loss, test_num_examples, test_metrics)
                 test_results.append((client_proxy, test_eval_res))
 
-            val_eval_res = EvaluateRes(
-                eval_res.status, eval_res.loss, eval_res.num_examples, val_metrics
-            )
+            val_eval_res = EvaluateRes(eval_res.status, eval_res.loss, eval_res.num_examples, val_metrics)
             val_results.append((client_proxy, val_eval_res))
 
         return val_results, test_results
@@ -656,9 +596,7 @@ class FlServer(Server):
             for key, value in test_metrics_aggregated.items():
                 val_metrics_aggregated[key] = value
             if test_loss_aggregated is not None:
-                val_metrics_aggregated[
-                    f"{MetricPrefix.TEST_PREFIX.value} loss - aggregated"
-                ] = test_loss_aggregated
+                val_metrics_aggregated[f"{MetricPrefix.TEST_PREFIX.value} loss - aggregated"] = test_loss_aggregated
 
         return val_loss_aggregated, val_metrics_aggregated
 
@@ -700,8 +638,6 @@ class FlServer(Server):
             len(failures),
         )
 
-        val_loss_aggregated, val_metrics_aggregated = self._handle_result_aggregation(
-            server_round, results, failures
-        )
+        val_loss_aggregated, val_metrics_aggregated = self._handle_result_aggregation(server_round, results, failures)
 
         return val_loss_aggregated, val_metrics_aggregated, (results, failures)
